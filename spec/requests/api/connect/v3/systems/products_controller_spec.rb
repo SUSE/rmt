1b7badd8b72a4349febe6703de7fc06acb540ed4
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2017-07-07 14:38:18 +0200

    Create shared examples; add some more tests

--- spec/requests/api/connect/v3/systems/products_controller_spec.rb
+++ spec/requests/api/connect/v3/systems/products_controller_spec.rb
@@ -7,35 +7,11 @@ RSpec.describe Api::Connect::V3::Systems::ProductsController, type: :request do
   let(:url) { connect_systems_products_url }
   let(:headers) { auth_header.merge(version_header) }
   let(:system) { FactoryGirl.create(:system) }
-  let(:product_without_repos) { FactoryGirl.create(:product) }
   let(:product_with_repos) { FactoryGirl.create(:product, :with_repositories) }
 
   describe '#activate' do
-    context 'when no credentials are provided' do
-      before { post url }
-      subject { response }
-
-      its(:code) { is_expected.to eq '401' }
-    end
-
-    context 'when required parameters are missing' do
-      it 'raises an error' do
-        expect { post url, headers: headers }.to raise_error ActionController::ParameterMissingTranslated
-      end
-    end
-
-    context 'when product has no repos' do
-      let(:payload) do
-        {
-            identifier: product_without_repos.identifier,
-            version: product_without_repos.version,
-            arch: product_without_repos.arch
-        }
-      end
-
-      it 'raises an error' do
-        expect { post url, headers: headers, params: payload }.to raise_error(/No repositories found for product/)
-      end
+    it_behaves_like 'products controller action' do
+      let(:verb) { 'post' }
     end
 
     context 'when product has repos' do
@@ -63,9 +39,7 @@ RSpec.describe Api::Connect::V3::Systems::ProductsController, type: :request do
       its(:body) { is_expected.to eq(serialized_json) }
 
       describe 'JSON response' do
-        let(:json) { JSON.parse(response.body, symbolize_names: true) }
-
-        subject { json }
+        subject { JSON.parse(response.body, symbolize_names: true) }
         it { is_expected.to include :id, :name, :product, :url, :obsoleted_service_name }
       end
     end
@@ -74,32 +48,8 @@ RSpec.describe Api::Connect::V3::Systems::ProductsController, type: :request do
   describe '#show' do
     let(:activation) { FactoryGirl.create(:activation) }
 
-    context 'when no credentials are provided' do
-      before { get url }
-      subject { response }
-
-      its(:code) { is_expected.to eq '401' }
-    end
-
-    context 'when required parameters are missing' do
-      it 'raises an error' do
-        expect { get url, headers: headers }.to raise_error ActionController::ParameterMissingTranslated
-      end
-    end
-
-    context 'when product does not exist' do
-      let(:payload) do
-        {
-            identifier: -1,
-            version: product_with_repos.version,
-            arch: product_with_repos.arch
-        }
-      end
-
-      before { get url, headers: headers, params: payload }
-      subject { response }
-
-      its(:code) { is_expected.to eq('422') }
+    it_behaves_like 'products controller action' do
+      let(:verb) { 'get' }
     end
 
     context 'when product is not activated' do
@@ -115,6 +65,11 @@ RSpec.describe Api::Connect::V3::Systems::ProductsController, type: :request do
       subject { response }
 
       its(:code) { is_expected.to eq('422') }
+
+      describe 'JSON response' do
+        subject { JSON.parse(response.body, symbolize_names: true) }
+        its([:error]) { is_expected.to match(/The requested product '.*' is not activated on this system/) }
+      end
     end
 
     context 'when product is activated' do
