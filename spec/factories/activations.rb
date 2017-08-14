7a84f2a1f73c24723c6b0bf0280fbd356fe64ac5
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2017-08-14 15:24:04 +0200

    Fix specs broken by non-mirrored repos

--- spec/factories/activations.rb
+++ spec/factories/activations.rb
@@ -8,5 +8,11 @@ FactoryGirl.define do
         activation.service.product.update_attribute(:free, true)
       end
     end
+
+    trait :with_mirrored_product do
+      after :build do |activation, _evaluator|
+        activation.service.product.repositories.update_all(mirroring_enabled: true)
+      end
+    end
   end
 end
