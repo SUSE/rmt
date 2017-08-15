2904cb9f65aadc863787ffdf0192ef01d145778f
Thomas Hutterer <thutterer@suse.de>
2017-08-15 18:17:19 +0200

    switch rmt-repos to use Thor version

--- lib/rmt/repo_manager.rb
+++ lib/rmt/repo_manager.rb
@@ -1,102 +1,67 @@
 require 'rmt'
 require 'optparse'
+require 'thor'
 
-class RMT::RepoManager
+class RMT::RepoManager < Thor
+  # class_option :verbose, type: :boolean
 
-  def initialize(argv, out = nil)
-    @argv = argv
-    @options = {}
-    @affected_repos = 0
-    @out = out || StringIO.new
+  desc "list", "List all repositories"
+  option :all, type: :boolean
+  def list
+    scope = options[:all] ? :all : :enabled
+    list_repositories(scope: scope)
   end
 
-  def parse_options
-    opt_parser = OptionParser.new do |opts|
-      opts.banner = 'Usage: rmt-repos [options]'
-
-      opts.separator ''
-
-      opts.on_tail('-h', '--help', 'Show this message') do
-        @out.puts opts
-        exit
-      end
-
-      opts.on_tail('-v', '--version', 'Show version') do
-        @out.puts RMT::VERSION
-        exit
-      end
-
-      opts.on('-a', '--all', 'List all repositories') do |all|
-        @options[:all] = all
-      end
-
-      opts.on('-e', '--enable', 'Enable the repository') do |opt|
-        @options[:enable] = opt
-      end
-
-      opts.on('-d', '--disable', 'Disable the repository') do |opt|
-        @options[:disable] = opt
-      end
-
-      opts.on('-r', '--repository REPOSITORY_ID', 'Repository by ID to enable/disable') do |opt|
-        @options[:repository] = opt
-      end
-
-      opts.on('-p', '--product PRODUCT', 'Product string to enable/disable in "identifier/version/arch" format') do |opt|
-        @options[:product] = true
-        @options[:identifier], @options[:version], @options[:arch] = opt.split('/')
-        unless (@options[:identifier] && @options[:version])
-          raise 'Please specify product string in "identifier/version/arch" format'
-        end
-      end
-
-      opts.on('-x', '--exclude-optional', 'Exclude non-mandatory repositories') do |opt|
-        @options[:exclude_optional] = opt
-      end
-    end
-
-    opt_parser.parse!(@argv)
-  end
-
-  def execute!
-    parse_options
-
-    if (@options[:enable] || @options[:disable])
-      mirroring_enabled = (@options[:enable]) ? true : false
-
-      if (@options[:repository])
-        change_repository_mirroring(mirroring_enabled, @options[:repository])
-      elsif (@options[:product])
-        change_product_mirroring(mirroring_enabled, @options[:identifier], @options[:version], @options[:arch])
-      else
-        raise 'Need to specify which repository to enable/disable!'
-      end
-
-      @out.puts "#{@affected_repos} repo(s) successfully #{mirroring_enabled ? 'enabled' : 'disabled'}"
+  desc "disable TARGET", "Disable a repository or product"
+  def disable(target)
+    repo_id = Integer(target, 10) rescue nil
+    if repo_id
+      change_repository_mirroring(false, repo_id)
     else
-      list_repositories
+      identifier, version, arch = target.split('/')
+      change_product_mirroring(false, identifier, version, arch)
     end
   end
 
-  # protected
+  desc "enable TARGET", "Enable a repository or product"
+  option 'exclude-optional', aliases: '-x', default: true, type: :boolean
+  def enable(target)
+    repo_id = Integer(target, 10) rescue nil
+    if repo_id
+      change_repository_mirroring(true, repo_id)
+    else
+      identifier, version, arch = target.split('/')
+      change_product_mirroring(true, identifier, version, arch, exclude_optional: options['exclude-optional'])
+    end
+  end
+
+  desc "version", "Show version"
+  def version
+    puts RMT::VERSION
+  end
+
+
+  protected
 
   def change_repository_mirroring(mirroring_enabled, repository_id)
     repository = Repository.find(repository_id)
     repository.mirroring_enabled = mirroring_enabled
     repository.save!
-    @affected_repos += 1
+    puts "Repo successfully #{mirroring_enabled ? 'enabled' : 'disabled'}."
   end
 
   def change_product_mirroring(mirroring_enabled, identifier, version, arch, options = {})
     conditions = { identifier: identifier, version: version }
     conditions[:arch] = arch if arch
+    repo_count = 0
 
     products = Product.where(conditions).all
     products.each do |product|
-      conditions = {}
+      conditions = { mirroring_enabled: !mirroring_enabled } # to only update the repos which need change
       conditions[:enabled] = true if (options[:exclude_optional])
-      @affected_repos += product.repositories.where(conditions).update_all(mirroring_enabled: mirroring_enabled)
+      repo_count += product.repositories.where(conditions).update_all(mirroring_enabled: mirroring_enabled)
     end
+    puts "#{repo_count} repo(s) successfully #{mirroring_enabled ? 'enabled' : 'disabled'}."
   end
 
   def list_repositories(scope: :enabled)
@@ -109,7 +74,7 @@ class RMT::RepoManager
       rows << [ repository.id, repository.name, repository.description, repository.mirroring_enabled ]
     end
 
-    @out.puts Terminal::Table.new headings: %w[ID Name Description Mirror?], rows: rows
+    puts Terminal::Table.new headings: %w[ID Name Description Mirror?], rows: rows
   end
 
 end
