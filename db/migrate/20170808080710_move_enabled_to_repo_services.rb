7de402ec0b31dcca9e6be7f374e178dce88765ea
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2017-08-16 10:58:41 +0200

    Fix migration to fill in local_path

--- db/migrate/20170808080710_move_enabled_to_repo_services.rb
+++ db/migrate/20170808080710_move_enabled_to_repo_services.rb
@@ -3,7 +3,12 @@ class MoveEnabledToRepoServices < ActiveRecord::Migration[5.1]
   def up
     add_column :repositories, :installer_updates, :boolean, null: false, default: false
     add_column :repositories, :mirroring_enabled, :boolean, null: false, default: false
-    add_column :repositories, :local_path, :string, null: false
+    add_column :repositories, :local_path, :string
+    Repository.all.each do |repo|
+      repo.local_path = Repository.make_local_path(repo.external_url)
+      repo.save!
+    end
+    change_column :repositories, :local_path, :string, null: false
     remove_index :repositories, name: 'index_repositories_on_name_and_distro_target'
     add_index :repositories, :external_url, unique: true
   end
