e107375bf4a742bf25405e21cd6c5bd6b16a748f
Thomas Hutterer <thutterer@suse.de>
2017-08-17 12:20:39 +0200

    change behavior of thor's help output

--- lib/rmt/repo_manager.rb
+++ lib/rmt/repo_manager.rb
@@ -5,7 +5,30 @@ require 'thor'
 
 class RMT::RepoManager < Thor
 
-  # class_option :verbose, type: :boolean
+  class << self
+    # monkey patch for the output of the help command
+    # (removes the alphabetical sorting and adds some custom behavior)
+    def help(shell, subcommand = false)
+      list = printable_commands(true, subcommand)
+      Thor::Util.thor_classes_in(self).each do |klass|
+        list += klass.printable_commands(false)
+      end
+
+      list.reject! {|l| l[0].split[1] == 'help'}
+
+      if defined?(@package_name) && @package_name
+        shell.say "#{@package_name} commands:"
+      else
+        shell.say "Commands:"
+      end
+
+      shell.print_table(list, :indent => 2, :truncate => true)
+      shell.say
+      class_options_help(shell)
+
+      shell.say 'All commands can be run with -h (or --help) for more information.'
+    end
+  end
 
   desc 'list', 'List all repositories'
   option :all, type: :boolean
