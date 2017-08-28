e4d50cf56881ac9f0d131aa44fb85f8f6f16a0f7
Andrei Shaidurov <ashaidurov@suse.com>
2017-08-28 15:15:51 +0200

    Health check endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
 
-  include ActionController::HttpAuthentication::Basic::ControllerMethods
-  include ActionController::HttpAuthentication::Token::ControllerMethods
 
 end
