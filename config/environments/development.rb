f440b69b962e2d1a6dbfa43df3924e076f282137
Andrei Shaidurov <ashaidurov@suse.com>
2017-08-29 10:10:39 +0200

    Application cleanup; don't load ActionDispatch in executables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,34 +9,11 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
-  config.consider_all_requests_local = true
-
-  # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -45,5 +22,29 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.public_file_server.enabled = true
+  unless ENV['RAILS_EXECUTABLE']
+    # Show full error reports.
+    config.consider_all_requests_local = true
+
+    # Enable/disable caching. By default caching is disabled.
+    if Rails.root.join('tmp', 'caching-dev.txt').exist?
+      config.action_controller.perform_caching = true
+
+      config.cache_store = :memory_store
+      config.public_file_server.headers = {
+        'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      }
+    else
+      config.action_controller.perform_caching = false
+
+      config.cache_store = :null_store
+    end
+
+    # Don't care if the mailer can't send.
+    # config.action_mailer.raise_delivery_errors = false
+
+    # config.action_mailer.perform_caching = false
+
+    config.public_file_server.enabled = true
+  end
 end
