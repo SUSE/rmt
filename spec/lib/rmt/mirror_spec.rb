61cb08d9eb2af1592819e7a16e7f1595ecbc3c69
Thomas Schmidt <tschmidt@suse.de>
2017-09-05 10:25:14 +0200

    move tmp dir handling to around block

--- spec/lib/rmt/mirror_spec.rb
+++ spec/lib/rmt/mirror_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe RMT::Mirror do
   describe '#mirror' do
+    around do |example|
+      @tmp_dir = Dir.mktmpdir('rmt')
+      example.run
+      FileUtils.remove_entry(@tmp_dir)
+    end
+
     context 'without auth_token' do
       let(:rmt_mirror) do
         described_class.new(
@@ -13,13 +19,10 @@ RSpec.describe RMT::Mirror do
       end
 
       before do
-        @tmp_dir = Dir.mktmpdir('rmt')
-
         VCR.use_cassette 'mirroring' do
           rmt_mirror.mirror
         end
       end
-      after { FileUtils.remove_entry(@tmp_dir) }
 
       it 'downloads rpm files' do
         rpm_entries = Dir.entries(File.join(@tmp_dir, 'dummy_repo')).select { |entry| entry =~ /\.rpm$/ }
@@ -44,13 +47,10 @@ RSpec.describe RMT::Mirror do
       end
 
       before do
-        @tmp_dir = Dir.mktmpdir('rmt')
-
         VCR.use_cassette 'mirroring_with_auth_token' do
           rmt_mirror.mirror
         end
       end
-      after { FileUtils.remove_entry(@tmp_dir) }
 
       it 'downloads rpm files' do
         rpm_entries = Dir.entries(File.join(@tmp_dir, 'dummy_repo')).select { |entry| entry =~ /\.rpm$/ }
@@ -75,13 +75,10 @@ RSpec.describe RMT::Mirror do
       end
 
       before do
-        @tmp_dir = Dir.mktmpdir('rmt')
-
         VCR.use_cassette 'mirroring_product' do
           rmt_mirror.mirror
         end
       end
-      after { FileUtils.remove_entry(@tmp_dir) }
 
       it 'downloads rpm files' do
         rpm_entries = Dir.entries(File.join(@tmp_dir, 'dummy_product/product/')).select { |entry| entry =~ /\.rpm$/ }
