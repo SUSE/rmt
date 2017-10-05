fe541849d02557918baae3b510a4302fb8195db5
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2017-10-05 11:41:22 +0200

    Move repo deletion method to the model

--- lib/rmt/scc.rb
+++ lib/rmt/scc.rb
@@ -32,7 +32,7 @@ class RMT::SCC
       update_auth_token(item)
     end
 
-    remove_suse_repos_without_tokens
+    Repository.remove_suse_repos_without_tokens!
 
     @logger.info('Updating subscriptions')
     data = scc_api_client.list_subscriptions
@@ -112,10 +112,6 @@ class RMT::SCC
       subscription_product_class.product_class = item_class
       subscription_product_class.save!
     end
-  end
-
-  def remove_suse_repos_without_tokens
-    Repository.where(auth_token: nil).where('external_url LIKE ?', 'https://updates.suse.com%').delete_all
   end
 
 end
