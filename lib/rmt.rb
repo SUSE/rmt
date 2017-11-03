8c0cfbf8a6bb11a4e1d4af995540d979e61b1668
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2017-11-03 13:43:00 +0100

    Use relative path in DEFAULT_MIRROR_DIR

--- lib/rmt.rb
+++ lib/rmt.rb
@@ -3,7 +3,7 @@ module RMT
 
   DEFAULT_USER = '_rmt'.freeze
   DEFAULT_GROUP = 'nginx'.freeze
-  DEFAULT_MIRROR_DIR = '/usr/share/rmt/public/repo/'.freeze
+  DEFAULT_MIRROR_DIR = File.expand_path(File.join(__dir__, '../public/repo/')).freeze
   DEFAULT_MIRROR_URL_PREFIX = '/repo/'.freeze
 
 end
