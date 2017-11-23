dd0db2f218f6a87ff74e4501c95bc4d5f7223fac
Thomas Hutterer <thutterer@suse.de>
2017-11-23 16:34:09 +0100

    fix after renaming

--- lib/rmt/cli/scc.rb
+++ lib/rmt/cli/scc.rb
@@ -2,7 +2,7 @@ class RMT::CLI::SCC < RMT::CLI::Subcommand
 
   desc 'sync', 'Get latest SCC data'
   def sync
-    if Settings.air_gap.offline
+    if Settings.airgap.offline
       RMT::SCC.new(options).import(airgap_path)
     else
       RMT::SCC.new(options).sync
@@ -12,7 +12,7 @@ class RMT::CLI::SCC < RMT::CLI::Subcommand
   desc 'export', 'Store SCC data for offline usage'
   option :path, desc: 'Overwrite the configured path'
   def export
-    abort 'This RMT is in offline-mode and cannot export SCC data.' if Settings.air_gap.offline
+    abort 'This RMT is in offline-mode and cannot export SCC data.' if Settings.airgap.offline
     RMT::SCC.new(options).export(airgap_path)
   end
 
