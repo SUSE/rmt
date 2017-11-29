65b15063d047ceef37004a36f2e5948f49514f4d
Thomas Hutterer <thutterer@suse.de>
2017-11-29 17:39:06 +0100

    add specs for import commands

---
+++ spec/lib/rmt/cli/import_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe RMT::CLI::Import do
+  let(:path) { '/mnt/usb' }
+
+  describe 'data' do
+    it 'calls sync with special params' do
+      expect_any_instance_of(RMT::SCC).to receive(:import).with(path)
+      described_class.start(['data', path])
+    end
+  end
+
+  describe 'repos' do
+    it 'mirrors enabled repos from path' do
+      expect_any_instance_of(RMT::CLI::Mirror).to receive(:mirror).with({ from_dir: path })
+      described_class.start(['repos', path])
+    end
+  end
+end
