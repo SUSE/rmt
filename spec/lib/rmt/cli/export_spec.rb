64be2dbafda1c1db474345789915055dc5841a51
Thomas Hutterer <thutterer@suse.de>
2017-11-29 17:32:54 +0100

    add specs for export commands

---
+++ spec/lib/rmt/cli/export_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe RMT::CLI::Export do
+  let(:path) { '/mnt/usb' }
+
+  describe 'settings' do
+    it 'writes ids of enabled repos to file' do
+      create :repository, mirroring_enabled: true, id: 123
+      create :repository, mirroring_enabled: false
+      expect(File).to receive(:write).with("#{path}/repos.json", '[123]')
+      described_class.start(['settings', path])
+    end
+  end
+
+  describe 'data' do
+    it 'calls sync with special params' do
+      expect_any_instance_of(RMT::SCC).to receive(:export).with(path)
+      described_class.start(['data', path])
+    end
+  end
+
+  describe 'repos' do
+    let(:repo_ids_from_file) { [42, 69] }
+    let(:repos_from_file) do
+      repo_ids_from_file.map { |id| create :repository, id: id }
+    end
+
+    before { create :repository, id: 666, mirroring_enabled: true }
+
+    it 'reads repo ids from file at path and mirrors these repos' do
+      expect(File).to receive(:read).with("#{path}/repos.json").and_return repo_ids_from_file.to_json
+      expect_any_instance_of(RMT::CLI::Mirror).to receive(:mirror).with({ base_dir: path, repos: repos_from_file })
+      described_class.start(['repos', path])
+    end
+  end
+end
