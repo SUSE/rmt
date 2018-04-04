2db1c429327d0eb3cf75a9e29ad40d47f195be66
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-04-04 12:47:32 +0200

    Allow configuring VCR via metadata

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :typhoeus
   c.allow_http_connections_when_no_cassette = true
+  c.configure_rspec_metadata!
 end
