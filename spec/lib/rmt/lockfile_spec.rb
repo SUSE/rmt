4a2fa5c7820e34ac28937484c52a9b3ba802e5aa
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2018-05-15 16:11:36 +0200

    Add :with_fakefs around filter

--- spec/lib/rmt/lockfile_spec.rb
+++ spec/lib/rmt/lockfile_spec.rb
@@ -3,31 +3,25 @@ require 'rails_helper'
 RSpec.describe RMT::Lockfile do
   let(:lockfile) { RMT::Lockfile::LOCKFILE_LOCATION }
 
-  describe '#lock' do
+  describe '#lock', :with_fakefs do
     subject(:lock) { described_class.lock { nil } }
 
     context 'without file' do
       it 'does not raise exception' do
-        FakeFS.with_fresh do
-          expect { lock }.not_to raise_error
-        end
+        expect { lock }.not_to raise_error
       end
 
       it 'creates a file and locks it' do
-        FakeFS.with_fresh do
-          allow(File).to receive(:open).with(lockfile, 66).exactly(1).times.and_call_original
-          expect_any_instance_of(File).to receive(:flock).exactly(3).times.and_call_original
-          lock
-        end
+        allow(File).to receive(:open).with(lockfile, 66).exactly(1).times.and_call_original
+        expect_any_instance_of(File).to receive(:flock).exactly(1).times.and_call_original
+        lock
       end
     end
 
     context 'with locked file' do
       it 'raises exception' do
-        FakeFS.with_fresh do
-          expect_any_instance_of(File).to receive(:flock).exactly(1).times.and_return(false)
-          expect { lock }.to raise_error(RMT::Lockfile::ExecutionLockedError)
-        end
+        expect_any_instance_of(File).to receive(:flock).exactly(1).times.and_return(false)
+        expect { lock }.to raise_error(RMT::Lockfile::ExecutionLockedError)
       end
     end
   end
--- spec/support/fakefs.rb
+++ spec/support/fakefs.rb
@@ -3,14 +3,3 @@ class FakeFS::File
     42 # this is a stub. see https://www.rubydoc.info/github/defunkt/fakefs/FakeFS/File
   end
 end
-
-# this makes lockfile dir to be present in cleaned FakeFS setup
-module FakeFS
-  class << self
-    def with_fresh(&block)
-      clear!
-      FileUtils.mkdir_p(File.dirname(RMT::Lockfile::LOCKFILE_LOCATION))
-      with(&block)
-    end
-  end
-end
