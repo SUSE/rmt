b90922b5fd0b3a12634412d5c94ee69d73dbc25f
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2018-06-05 16:16:15 +0200

    Log versions in journald logs

--- lib/rmt/logger.rb
+++ lib/rmt/logger.rb
@@ -1,12 +1,15 @@
 class RMT::Logger < ActiveSupport::Logger
   def initialize(dest)
     super(dest)
-    self.formatter = if ENV['LOG_TO_JOURNALD'].present?
-                       proc do |severity, _time, _progname, msg|
-                         "#{severity}: #{msg}\n"
-                       end
-                     else
-                       ::Logger::Formatter.new
-                     end
+
+    # LOG_TO_JOURNALD is set by the systemd timers in order to remove duplicate timestamps in the logs
+    if ENV['LOG_TO_JOURNALD'].present?
+      self.formatter = proc do |severity, _time, _progname, msg|
+       "#{severity}: #{msg}\n"
+      end
+      self.info "RMT version #{RMT::VERSION}"
+    else
+      self.formatter = ::Logger::Formatter.new
+    end
   end
 end
