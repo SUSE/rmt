2ae8e78abd6e4477f6baf06aa31c99e641d66c89
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2018-06-06 10:35:20 +0200

    Make Danger happy

--- lib/rmt/logger.rb
+++ lib/rmt/logger.rb
@@ -4,10 +4,8 @@ class RMT::Logger < ActiveSupport::Logger
 
     # LOG_TO_JOURNALD is set by the systemd timers in order to remove duplicate timestamps in the logs
     if ENV['LOG_TO_JOURNALD'].present?
-      self.formatter = proc do |severity, _time, _progname, msg|
-       "#{severity}: #{msg}\n"
-      end
-      self.info "RMT version #{RMT::VERSION}"
+      self.formatter = proc { |severity, _time, _progname, msg| "#{severity}: #{msg}\n" }
+      info "RMT version #{RMT::VERSION}"
     else
       self.formatter = ::Logger::Formatter.new
     end
