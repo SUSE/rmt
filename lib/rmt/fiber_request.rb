a1dd0fb5f456caa094be5a26f3d9d3bc7025ade7
Ivan Kapelyukhin <ikapelyukhin@users.noreply.github.com>
2018-09-26 16:16:39 +0200

    Emulate on_headers callback when the request failed

--- lib/rmt/fiber_request.rb
+++ lib/rmt/fiber_request.rb
@@ -17,6 +17,7 @@ class RMT::FiberRequest < RMT::HttpRequest
       @download_path.write(chunk)
     end
     on_complete do |response|
+      @request_fiber.resume(response) unless (response.return_code == :ok)
       @request_fiber.resume(response) if @request_fiber.alive?
     end
   end
