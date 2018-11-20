dfb382a09872be69c61d843b55a37d86c6b4160a
Thomas Muntaner <tmuntaner@suse.de>
2018-11-20 15:55:43 +0100

    Cleaner i18n boot for cli apps

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,5 +1,6 @@
 # Configure fast_gettext for i18n.
-FastGettext.add_text_domain('rmt', path: Rails.root.join('locale'), type: :po, ignore_fuzzy: true, report_warning: false)
-FastGettext.default_locale = I18n.default_locale
-FastGettext.default_available_locales = %w[en de fr ja es pt_BR zh_CN]
-FastGettext.default_text_domain = 'rmt'
+rmt_path = File.expand_path('../../', __dir__)
+FastGettext.add_text_domain('rmt', path: File.join(rmt_path, 'locale'), type: :po, ignore_fuzzy: true, report_warning: false)
+FastGettext.default_locale = :en
+FastGettext.available_locales = FastGettext.default_available_locales = %w[en de fr ja es pt_BR zh_CN]
+FastGettext.text_domain = FastGettext.default_text_domain = 'rmt'
