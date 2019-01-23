e53a8ff32008a245cac48f43f19d489d4dc01af1
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2019-01-23 16:38:42 +0100

    Add specs for legacy service XML

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,16 +11,14 @@ class ServicesController < ApplicationController
   ZYPPER_SERVICE_TTL = 86400
 
   def show
-    @service    = Service.find(params[:id])
-    @repos      = @service.repositories
-    @expires_at = nil
-    @base_url   = request.base_url
+    service = Service.find(params[:id])
+    repos = service.repositories
 
     builder = Builder::XmlMarkup.new
     service_xml = builder.repoindex(ttl: ZYPPER_SERVICE_TTL) do
-      @repos.each do |repo|
+      repos.each do |repo|
         attributes = {
-          url: RMT::Misc.make_repo_url(@base_url, repo.local_path, @service.name),
+          url: RMT::Misc.make_repo_url(request.base_url, repo.local_path, service.name),
           alias: repo.name,
           name: repo.name,
           autorefresh: repo.autorefresh,
@@ -38,13 +36,17 @@ class ServicesController < ApplicationController
   end
 
   def legacy_service
-    @repos = Repository.joins(services: :activations).where('activations.system_id' => @system.id, mirroring_enabled: true)
+    repos = Repository.joins(services: :activations).where('activations.system_id' => @system.id, mirroring_enabled: true)
 
     builder = Builder::XmlMarkup.new
     service_xml = builder.repoindex(ttl: ZYPPER_SERVICE_TTL) do
-      @repos.each do |repo|
+      repos.each do |repo|
         attributes = {
-          url: make_repo_url(request, repo.local_path, make_smt_service_name(request.base_url)),
+          url: RMT::Misc.make_repo_url(
+            request.base_url,
+            repo.local_path,
+            RMT::Misc.make_smt_service_name(request.base_url)
+          ),
           alias: repo.name,
           name: repo.name,
           autorefresh: repo.autorefresh,
@@ -56,16 +58,6 @@ class ServicesController < ApplicationController
     end
 
     render xml: service_xml
-  end
-
-  protected
-
-  def make_repo_url(request, repo_local_path, service_name)
-    RMT::Misc.make_repo_url(request.base_url, repo_local_path, service_name)
-  end
-
-  def make_smt_service_name(url)
-    "SMT-#{url}".gsub!(%r{:*/+}, '_').tr('.', '_').gsub(/_$/, '')
   end
 
 end
