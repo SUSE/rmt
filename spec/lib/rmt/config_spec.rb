603a910468951d3241018d058643028dd7095b72
Thomas Muntaner <thomas.muntaner@gmail.com>
2019-08-16 10:54:06 +0200

    Fix mirror_src config
    We took and documented the configuration value, but never actually used it.

--- spec/lib/rmt/config_spec.rb
+++ spec/lib/rmt/config_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe RMT::Config do
+  describe '#mirroring mirror_src' do
+    context 'defaults' do
+      [nil, ''].each do |config_provided|
+        before { Settings['mirroring'].mirror_src = config_provided }
+        it("defaults when supplied #{config_provided}") { expect(described_class.mirror_src_files?).to be_falsey }
+      end
+    end
+
+    context 'true' do
+      [true, 'true'].each do |config_provided|
+        before { Settings['mirroring'].mirror_src = config_provided }
+        it("defaults when supplied #{config_provided}") { expect(described_class.mirror_src_files?).to be_truthy }
+      end
+    end
+
+    context 'false' do
+      [false, 'false'].each do |config_provided|
+        before { Settings['mirroring'].mirror_src = config_provided }
+        it("defaults when supplied #{config_provided}") { expect(described_class.mirror_src_files?).to be_falsey }
+      end
+    end
+  end
+
   describe '#mirroring dedup_method' do
     context 'defaults' do
       [nil, ''].each do |dedup_method|
