95dd804cef95076ac06db5c11e2685ec994cb54b
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2019-10-23 09:26:30 +0200

    Use regular logger; bump the version

--- engines/zypper_auth/lib/zypper_auth/engine.rb
+++ engines/zypper_auth/lib/zypper_auth/engine.rb
@@ -1,7 +1,7 @@
 module ZypperAuth
   class << self
     def auth_logger
-      @logger ||= RMT::Logger.new('/var/lib/rmt/zypper_auth.log')
+      @logger ||= ::Logger.new('/var/lib/rmt/zypper_auth.log')
       @logger.reopen
       @logger
     end
