7c7a5c3919083ab1b26f94f2dc091a5611db3486
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-01-29 11:06:54 -0300

    Remove RMT::Rpm::RepomdXmlParser files
    After replacing the RMT::Rpm::RepomdXmlParser with the repomd_parser
    gem, there is no need to keep the local lib code on the project.

--- lib/rmt/rpm.rb
+++ lib/rmt/rpm.rb
@@ -5,6 +5,5 @@ end
 
 require_relative 'rpm/file_entry'
 require_relative 'rpm/base_parser'
-require_relative 'rpm/repomd_xml_parser'
 require_relative 'rpm/deltainfo_xml_parser'
 require_relative 'rpm/primary_xml_parser'
