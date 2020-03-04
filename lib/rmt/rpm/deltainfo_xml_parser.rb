ea52f226d90989a8ae163e7c1d1a91672ef9ea40
Natnael Getahun <connect@ngetahun.me>
2020-03-04 15:27:14 +0100

    Use Repomd Delta Parser

--- lib/rmt/rpm/deltainfo_xml_parser.rb
+++ lib/rmt/rpm/deltainfo_xml_parser.rb
@@ -1,37 +1,16 @@
-class RMT::Rpm::DeltainfoXmlParser < RMT::Rpm::BaseParser
-
+class RMT::Rpm::DeltainfoXmlParser < RepomdParser::DeltainfoXmlParser
+  attr_reader :referenced_files
   def initialize(filename, mirror_src = false)
     super(filename)
     @mirror_src = mirror_src
   end
 
-  def start_element(name, attrs = [])
-    @current_node = name.to_sym
-    if (name == 'newpackage')
-      @package = {}
-      @package[:version] = get_attribute(attrs, 'version')
-      @package[:name] = get_attribute(attrs, 'name')
-      @package[:arch] = get_attribute(attrs, 'arch')
-    elsif (name == 'delta')
-      @delta = {}
-    elsif (name == 'checksum')
-      @delta[:checksum_type] = get_attribute(attrs, 'type')
-    end
-  end
-
-  def characters(string)
-    if (@current_node == :filename)
-      @delta[:location] ||= ''
-      @delta[:location] += string.strip
-    elsif (@current_node == :checksum)
-      @delta[:checksum] ||= ''
-      @delta[:checksum] += string.strip
-    end
-  end
-
   def end_element(name)
     if (name == 'delta')
-      unless (@package[:arch] == 'src' && !@mirror_src)
+      if (@package[:arch] == 'src' && !@mirror_src)
+        # Call the end_element from RepomdParser::DeltainfoXmlParser
+        super.end_element(name)
+      else
         @referenced_files << RMT::Rpm::FileEntry.new(
           @delta[:location],
           @delta[:checksum_type],
