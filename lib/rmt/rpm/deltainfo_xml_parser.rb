38e9499bb35ecfdb86720f1a062de34254e1d7b1
Natnael Getahun <connect@ngetahun.me>
2020-03-05 09:54:12 +0100

    Fix: Improve code coverage and suppress redundant logic

--- lib/rmt/rpm/deltainfo_xml_parser.rb
+++ lib/rmt/rpm/deltainfo_xml_parser.rb
@@ -6,20 +6,33 @@ class RMT::Rpm::DeltainfoXmlParser < RepomdParser::DeltainfoXmlParser
   end
 
   def end_element(name)
-    if (name == 'delta')
-      if (@package[:arch] == 'src' && !@mirror_src)
-        # Call the end_element from RepomdParser::DeltainfoXmlParser
-        super.end_element(name)
-      else
-        @referenced_files << RepomdParser::Reference.new(
-          location: @delta[:location],
-          checksum_type: @delta[:checksum_type],
-          checksum: @delta[:checksum],
-          type: :drpm,
-          size: @delta[:size].to_i || nil
-        )
-      end
+    return unless delta? name
+
+    unless delta_src_package_with_mirror_src?
+      build_reference_for_package_with_src(@delta)
+      return
     end
+
+    super.end_element(name)
   end
 
+  private
+
+  def delta?(name)
+    name == 'delta'
+  end
+
+  def delta_src_package_with_mirror_src?
+    (@package[:arch] == 'src' && !@mirror_src)
+  end
+
+  def build_reference_for_package_with_src(delta)
+    @referenced_files << RepomdParser::Reference.new(
+      location: delta[:location],
+       checksum_type: delta[:checksum_type],
+       checksum: delta[:checksum],
+       type: :drpm,
+       size: delta[:size].to_i || nil
+     )
+  end
 end
--- lib/rmt/rpm/primary_xml_parser.rb
+++ lib/rmt/rpm/primary_xml_parser.rb
@@ -6,20 +6,33 @@ class RMT::Rpm::PrimaryXmlParser < RepomdParser::PrimaryXmlParser
   end
 
   def end_element(name)
-    if (name == 'package')
-      if (@package[:arch] == 'src' && !@mirror_src)
-        # Call Parent class end_element in general cases
-        super.end_element(name)
-      else
-        @referenced_files << RepomdParser::Reference.new(
-          location: @package[:location],
-          checksum_type: @package[:checksum_type],
-          checksum: @package[:checksum],
-             type: :rpm,
-             size: @package[:size].to_i || nil
-       )
-      end
+    return unless package? name
+
+    unless primary_src_package_with_mirror_src?
+      build_reference_for_package_with_src(@package)
+      return
     end
+
+    super.end_element(name)
   end
 
+  private
+
+  def package?(name)
+    name == 'package'
+  end
+
+  def primary_src_package_with_mirror_src?
+    @package[:arch] == 'src' && !@mirror_src
+  end
+
+  def build_reference_for_package_with_src(package)
+    @referenced_files << RepomdParser::Reference.new(
+      location: package[:location],
+       checksum_type: package[:checksum_type],
+       checksum: package[:checksum],
+       type: :rpm,
+       size: package[:size].to_i || nil
+      )
+  end
 end
