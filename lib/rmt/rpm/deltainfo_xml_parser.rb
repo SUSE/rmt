5959d7e5f349da0d2dbe2c9d2921934663815025
Ihtisham ul Haq <iulhaq@suse.de>
2020-03-05 23:20:45 +0100

    Make use of `repomd-parser` for DeltainfoXmlParser
    * Inherit from RepomdParser::DeltainfoXmlParser
    * Override in the new class to incorporate 'mirror_src' feature
    * Update test cases and fixtures

--- lib/rmt/rpm/deltainfo_xml_parser.rb
+++ lib/rmt/rpm/deltainfo_xml_parser.rb
@@ -1,43 +1,14 @@
-class RMT::Rpm::DeltainfoXmlParser < RMT::Rpm::BaseParser
+class RMT::Rpm::ModifiedDeltainfoXmlParser < RepomdParser::DeltainfoXmlParser
 
   def initialize(filename, mirror_src = false)
     super(filename)
     @mirror_src = mirror_src
   end
 
-  def start_element(name, attrs = [])
-    @current_node = name.to_sym
-    if (name == 'newpackage')
-      @package = {}
-      @package[:version] = get_attribute(attrs, 'version')
-      @package[:name] = get_attribute(attrs, 'name')
-      @package[:arch] = get_attribute(attrs, 'arch')
-    elsif (name == 'delta')
-      @delta = {}
-    elsif (name == 'checksum')
-      @delta[:checksum_type] = get_attribute(attrs, 'type')
-    end
-  end
-
-  def characters(string)
-    if (@current_node == :filename)
-      @delta[:location] ||= ''
-      @delta[:location] += string.strip
-    elsif (@current_node == :checksum)
-      @delta[:checksum] ||= ''
-      @delta[:checksum] += string.strip
-    end
-  end
-
   def end_element(name)
     if (name == 'delta')
       unless (@package[:arch] == 'src' && !@mirror_src)
-        @referenced_files << RMT::Rpm::FileEntry.new(
-          @delta[:location],
-          @delta[:checksum_type],
-          @delta[:checksum],
-          :drpm
-        )
+        super(name)
       end
     end
   end
--- spec/lib/rmt/rpm/deltainfo_xml_parser_spec.rb
+++ spec/lib/rmt/rpm/deltainfo_xml_parser_spec.rb
@@ -1,28 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe RMT::Rpm::DeltainfoXmlParser do
-  let(:parser) do
-    described_class.new(
-      file_fixture('dummy_repo/repodata/a546b430098b8a3fb7d65493a9ce608fafcb32f451d0ce8bf85410191f347cc3-deltainfo.xml.gz')
+RSpec.describe RMT::Rpm::ModifiedDeltainfoXmlParser do
+  let(:expected_result) do [
+    RepomdParser::Reference.new(
+      location: 'apples-0.1-0.x86_64.drpm',
+      checksum_type: 'sha256',
+      checksum: 'd5da95c8606a3de101d543e7d90c96f59b9f7cf50a8c944cbee889505401565e',
+      type: :drpm,
+      size: 2087,
+      arch: 'x86_64',
+    ),
+    RepomdParser::Reference.new(
+      location: 'oranges-0.1-0.x86_64.drpm',
+      checksum_type: 'sha256',
+      checksum: 'b0ec989937ef76c88cedb50848cc111bf2f3bcbb490fa8c8c1180aa4a9a63d73',
+      type: :drpm,
+      size: 2083,
+      arch: 'src',
     )
+  ]
   end
 
-  before { parser.parse }
+  context 'plain XML with mirror=false' do
+    let(:references) do
+      described_class.new(
+        file_fixture('dummy_repo/repodata/a546b430098b8a3fb7d65493a9ce608fafcb32f451d0ce8bf85410191f347cc3-deltainfo.xml.gz'),
+        mirror_src = false
+      ).parse
+    end
 
-  it 'references drpm files' do
-    expect(parser.referenced_files).to eq [
-      RMT::Rpm::FileEntry.new(
-        'apples-0.1-0.x86_64.drpm',
-        'sha256',
-        'd5da95c8606a3de101d543e7d90c96f59b9f7cf50a8c944cbee889505401565e',
-        :drpm
-      ),
-      RMT::Rpm::FileEntry.new(
-        'oranges-0.1-0.x86_64.drpm',
-        'sha256',
-        'b0ec989937ef76c88cedb50848cc111bf2f3bcbb490fa8c8c1180aa4a9a63d73',
-        :drpm
-      )
-    ]
+    it 'references drpm files with mirror=false' do
+      # the last element should be removed from the expected results as it has arch=src
+      expect(references).to eq(expected_result.first expected_result.size - 1)
+    end
+  end
+
+  context 'plain XML with mirror=true' do
+    let(:references) do
+      described_class.new(
+        file_fixture('dummy_repo/repodata/a546b430098b8a3fb7d65493a9ce608fafcb32f451d0ce8bf85410191f347cc3-deltainfo.xml.gz'),
+        mirror_src = true
+      ).parse
+    end
+
+    it 'references drpm files with mirror=true' do
+      # all the packages should be returned this time as mirror=true
+      # even though arch=src for the last package
+      expect(references).to eq(expected_result)
+    end
   end
 end
