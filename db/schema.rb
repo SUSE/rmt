34d1e8ebaf8f42588fdb2e53c63bc7dd989b914a
Felix Schnizlein <fschnizlein@suse.com>
2020-04-03 10:06:43 +0200

    Migrate provisional subscriptions to kind evaluation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205123840) do
+ActiveRecord::Schema.define(version: 20200403075822) do
 
   create_table "activations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "service_id", null: false
