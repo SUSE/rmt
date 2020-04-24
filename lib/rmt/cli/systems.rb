13b8303f6f81d7349145c144948804b0a84d5b43
Alex Palladii <alpalladii@gmail.com>
2020-04-24 18:36:25 +0200

    feature: completely remove a system by login [list below] - `rmt-cli systems remove command` - methods necessary to implement functionality - tests

--- lib/rmt/cli/systems.rb
+++ lib/rmt/cli/systems.rb
@@ -4,6 +4,9 @@ class RMT::CLI::Systems < RMT::CLI::Base
   option :all, aliases: '-a', type: :boolean, desc: _('List all registered systems')
   option :csv, type: :boolean, desc: _('Output data in CSV format')
 
+  class SystemNotFoundException < StandardError; end
+  class SystemNotDestroyedException < StandardError; end
+
   def list
     systems = (options.all ? System.all : System.limit(options.limit)).order(id: :desc)
     decorator = RMT::CLI::Decorators::SystemDecorator.new(systems)
@@ -28,4 +31,39 @@ class RMT::CLI::Systems < RMT::CLI::Base
     RMT::SCC.new(options).sync_systems
   end
 
+  desc 'remove TARGET', _('Permanently removes system with all its subscriptions and products.')
+  long_desc <<~REMOVE
+    #{_('Permanently removes selected system by login with all its subscriptions and products by login.')}
+
+    #{_('Examples')}:
+
+    $ rmt-cli systems remove uniqueLogin
+  REMOVE
+  def remove(target)
+    purge_system(target)
+  end
+
+  protected
+
+  def purge_system(target)
+    system = find_system(target)
+    destroy_system!(system)
+    puts _('Successfully removed system with login %{login}') % { login: target }
+  rescue SystemNotFoundException, SystemNotDestroyedException => e
+    puts e.message
+  end
+
+  private
+
+  def find_system(target)
+    System.find_by!(login: target)
+  rescue ActiveRecord::RecordNotFound
+    raise SystemNotFoundException.new(_('System with login %{login} not found.') % { login: target })
+  end
+
+  def destroy_system!(system)
+    system.destroy!
+  rescue ActiveRecord::RecordNotDestroyed
+    raise SystemNotDestroyedException.new(_('System with login %{login} cannot be removed') % { login: target })
+  end
 end
