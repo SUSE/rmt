4ebb72845daadddbd43d41c2ba2f8491c9d33c0f
Luis Caparroz <lcaparroz@suse.com>
2020-07-22 14:47:40 -0300

    Add logic to remove mismatched files/entries
    During mirroring, even though RMT checks if there are entries that match
    a given local path, it wouldn't remove duplicated entries or a
    mismatched file.
    
    Initially, the idea was to enforce uniqueness on the `local_path` field
    of the `downloaded_files` table, but it could cause disruption on any
    customer's database which has duplicated entries prior to it. Instead,
    we opted to check for duplicated entries only when mirroring a file.
    
    Then, I added some treatment for duplicated entries on the database and
    for mismatched existing files on disk.

--- app/models/downloaded_file.rb
+++ app/models/downloaded_file.rb
@@ -19,19 +19,26 @@ class DownloadedFile < ApplicationRecord
       return false unless File.exist?(path)
 
       valid_checksum = RMT::ChecksumVerifier.match_checksum?(checksum_type, checksum_value, path)
-      tracked_file = find_by(local_path: path)
-      matched_tracked_file = matches_tracked_file?(tracked_file, checksum_type, checksum_value)
+      file_db_entries = where(local_path: path)
+      matched_tracked_file = file_db_entries.find do |entry|
+        match_tracked_file?(entry, checksum_type, checksum_value)
+      end
+
+      duplicated_entries = file_db_entries.select { |entry| entry != matched_tracked_file }
+      duplicated_entries.each(&:destroy)
 
       return true if valid_checksum && matched_tracked_file
-      return !add_file(checksum_type, checksum_value, path).nil? if valid_checksum && tracked_file.nil?
+      return !add_file(checksum_type, checksum_value, path).nil? if valid_checksum && matched_tracked_file.nil?
 
-      tracked_file.destroy unless tracked_file.nil?
+      FileUtils.remove_file(path, force: true)
+      matched_tracked_file.destroy unless matched_tracked_file.nil?
+
       false
     end
 
     private
 
-    def matches_tracked_file?(tracked_file, checksum_type, checksum_value)
+    def match_tracked_file?(tracked_file, checksum_type, checksum_value)
       return false if tracked_file.nil? || tracked_file.checksum_type != checksum_type || tracked_file.checksum != checksum_value
       true
     end
--- spec/models/downloaded_file_spec.rb
+++ spec/models/downloaded_file_spec.rb
@@ -28,16 +28,30 @@ describe DownloadedFile, type: :model do
       expect(DownloadedFile.find_by(checksum_type: 'foobar', checksum: 'barfoo').file_size).to eq(File.size(test_file_2_path))
     end
 
-    it 'handles duplicates' do
+    it 'allows checksum duplicates' do
       add_downloaded_file('foo', 'bar', test_file_2_path)
-      expect(DownloadedFile.find_by(checksum_type: 'foo', checksum: 'bar').local_path).to eq(test_file_1_path)
+      expect(DownloadedFile.where(checksum_type: 'foo', checksum: 'bar').count).to eq(2)
     end
   end
 
   describe '#valid_local_file?' do
+    around do |example|
+      @tmp_dir = Dir.mktmpdir('rmt')
+      example.run
+      FileUtils.remove_entry(@tmp_dir)
+    end
+
     let(:checksum_type) { 'SHA256' }
     let(:checksum) { '5c4e3fa1624bd23251eecdda9c7fcefad045995a9eaed527d06dd8510cfe2851' }
-    let(:test_file_path) { file_fixture('dummy_product/product/apples-0.1-0.x86_64.rpm').to_s }
+    let(:test_file_path) do
+      fixture_relative_path = 'dummy_product/product/apples-0.1-0.x86_64.rpm'
+      fixture_path = file_fixture(fixture_relative_path).to_s
+
+      File.join(@tmp_dir, fixture_relative_path).tap do |file|
+        FileUtils.mkdir_p(File.dirname(file))
+        FileUtils.cp(fixture_path, file)
+      end
+    end
 
     it 'returns true if the file is valid' do
       add_downloaded_file(checksum_type, checksum, test_file_path)
@@ -62,11 +76,68 @@ describe DownloadedFile, type: :model do
       expect(DownloadedFile.where(local_path: test_file_path).count).to eq(1)
     end
 
-    it 'stops tracking invalid files' do
-      add_downloaded_file(checksum_type, checksum.sub('5', '2'), test_file_path)
-      expect(DownloadedFile.where(local_path: test_file_path).count).to eq(1)
-      expect(DownloadedFile.valid_local_file?(checksum_type, checksum, test_file_path)).to be(false)
-      expect(DownloadedFile.where(local_path: test_file_path).count).to eq(0)
+    context 'when a file matches a DB entry but not the checksum metadata' do
+      it 'stops tracking and remove invalid files' do
+        add_downloaded_file(checksum_type, checksum, test_file_path)
+
+        expect(DownloadedFile.where(local_path: test_file_path).count).to eq(1)
+        expect(File.exist?(test_file_path)).to be(true)
+
+        expect(DownloadedFile.valid_local_file?(checksum_type, checksum.sub('5', '2'), test_file_path)).to be(false)
+
+        expect(DownloadedFile.where(local_path: test_file_path).count).to eq(0)
+        expect(File.exist?(test_file_path)).to be(false)
+      end
+    end
+
+    context 'when there are duplicated entries (same local path) on db' do
+      context 'and at least one entry matches a valid local file and the checksum metadata' do
+        it 'removes other duplicated entries' do
+          add_downloaded_file(checksum_type, checksum, test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '2'), test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '3'), test_file_path)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(3)
+          expect(File.exist?(test_file_path)).to be(true)
+
+          expect(DownloadedFile.valid_local_file?(checksum_type, checksum, test_file_path)).to be(true)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(1)
+          expect(File.exist?(test_file_path)).to be(true)
+        end
+      end
+
+      context 'and at least one entry matches the checksum metadata but not the local file' do
+        it 'removes all duplicated entries and mismatched file' do
+          add_downloaded_file(checksum_type, checksum.sub('5', '1'), test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '2'), test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '3'), test_file_path)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(3)
+          expect(File.exist?(test_file_path)).to be(true)
+
+          expect(DownloadedFile.valid_local_file?(checksum_type, checksum.sub('5', '1'), test_file_path)).to be(false)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(0)
+          expect(File.exist?(test_file_path)).to be(false)
+        end
+      end
+
+      context 'and no entries match a local file neither the checksum metadata' do
+        it 'removes all duplicated entries and mismatched file' do
+          add_downloaded_file(checksum_type, checksum.sub('5', '1'), test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '2'), test_file_path)
+          add_downloaded_file(checksum_type, checksum.sub('5', '3'), test_file_path)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(3)
+          expect(File.exist?(test_file_path)).to be(true)
+
+          expect(DownloadedFile.valid_local_file?(checksum_type, checksum.sub('5', '4'), test_file_path)).to be(false)
+
+          expect(DownloadedFile.where(local_path: test_file_path).count).to eq(0)
+          expect(File.exist?(test_file_path)).to be(false)
+        end
+      end
     end
   end
 
