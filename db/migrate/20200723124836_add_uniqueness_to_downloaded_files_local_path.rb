bc3979a68a76fda7c3e24d06ae7e08b3164f38c7
Luis Caparroz <lcaparroz@suse.com>
2020-07-28 11:57:45 -0300

    Fix migration error on Travis CI
    The last Rails migration (add uniqueness to `local_path` on
    `downloaded_files`) is failing with the following error on Travis CI:
    
    Mysql2::Error: You can't specify target table 'downloaded_files' for
    update in FROM clause: DELETE FROM `downloaded_files` WHERE
    `downloaded_files`.`id` NOT IN (SELECT max(id) FROM `downloaded_files`
    GROUP BY `downloaded_files`.`local_path`)
    
    It seems some other databases (like MariaDB), accept this type of nested
    statements, but MySQL forbid them. One of the solutions I found by
    researching at the internet was to do a `INNER JOIN`, forcing MySQL to
    create a temporary table and them skip `derived merge optmization`.
    
    Reference links:
    
    * https://stackoverflow.com/questions/45494/mysql-error-1093-cant-specify-target-table-for-update-in-from-clause
    * https://mariadb.com/kb/en/derived-table-merge-optimization/

--- db/migrate/20200723124836_add_uniqueness_to_downloaded_files_local_path.rb
+++ db/migrate/20200723124836_add_uniqueness_to_downloaded_files_local_path.rb
@@ -1,8 +1,18 @@
 class AddUniquenessToDownloadedFilesLocalPath < ActiveRecord::Migration[5.2]
   def change
     # Remove duplicates before adding uniqueness to `local_path`
-    unique_ids = DownloadedFile.group(:local_path).select('max(id)')
-    DownloadedFile.where.not(id: unique_ids).delete_all
+    ActiveRecord::Base.connection.execute(
+      <<~SQL
+        DELETE FROM `downloaded_files`
+        WHERE `downloaded_files`.`id` NOT IN (
+            SELECT * FROM (
+                SELECT max(`files`.`id`) AS unique_id FROM `downloaded_files` AS `files`
+                INNER JOIN `downloaded_files` ON `downloaded_files`.`id`=`files`.`id`
+                GROUP BY `files`.`local_path`
+            ) as tmp
+        )
+      SQL
+    )
 
     add_index :downloaded_files, :local_path, unique: true
   end
