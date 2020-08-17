9923e93bbe845046fcbf4ceb682d79a0e6b7aa2d
Luis Caparroz <lcaparroz@suse.com>
2020-08-17 11:53:51 -0300

    Fix migration for downloaded_files.local_path
    The migration for adding index/uniqueness to
    `downloaded_files.local_path` didn't performed well on a test within a
    database with 300k+ records:
    
    * The `DELETE` statement would lock the DB for a considerable amount of
      time, which caused a lock wait timeout error (both using ActiveRecord
      or MariaDB console);
    * Even converting the statement to a `SELECT` didn't work, as the query
      would get stuck "forever".
    
    Since the ecosystem of RMT customers may considerable vary, we rewrote
    the migration to improve the query execution time and to delete one
    record a time, avoiding locking the DB for so long.
    
    Co-authored-by: Thomas Muntaner <thomas.muntaner@gmail.com>

--- db/migrate/20200723124836_add_uniqueness_to_downloaded_files_local_path.rb
+++ db/migrate/20200723124836_add_uniqueness_to_downloaded_files_local_path.rb
@@ -1,16 +1,36 @@
 class AddUniquenessToDownloadedFilesLocalPath < ActiveRecord::Migration[5.2]
   def change
     # Remove duplicates before adding uniqueness to `local_path`
-    ActiveRecord::Base.connection.execute(
+    logger = RMT::Logger.new(STDOUT)
+
+    logger.info(_('Finding `downloaded_files.local_path` duplicated records...'))
+    duplicated_records = DownloadedFile.find_by_sql(
       <<~SQL
-        DELETE df1 FROM
-            downloaded_files AS df1,
-            downloaded_files AS df2
-            WHERE df1.id < df2.id AND
-                df1.local_path = df2.local_path
+        SELECT df1.* FROM downloaded_files AS df1
+            JOIN(
+                SELECT local_path, max(id) AS max_id FROM downloaded_files
+                GROUP BY downloaded_files.local_path
+                HAVING (count(*) > 1)
+            ) AS df2
+            ON df1.local_path = df2.local_path
+        WHERE df1.id != df2.max_id
       SQL
     )
 
+    duplicated_count = duplicated_records.count
+    if duplicated_count > 0
+      logger.info(_("#{duplicated_count} duplicated records have been found"))
+      duplicated_records.each do |file|
+        logger.info(_("Removing record: ID #{file.id} with path '#{file.local_path}'"))
+        file.destroy
+      end
+    else
+      logger.info(_('No duplicated records has been found.'))
+    end
+
+    # Add index with uniqueness to `local_path`
+    logger.info(_('Adding index to `downloaded_file.local_path` with an uniqueness constraint...'))
+    logger.info(_('(This step can take some time to complete.)'))
     add_index :downloaded_files, :local_path, unique: true
   end
 end
