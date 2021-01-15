8c61327722f948b85e599627532b4832786ff603
ngetahun <natnael.getahun@suse.com>
2021-01-15 16:41:31 +0100

    Use correct no-recommends zypper flag

--- integration/install.sh
+++ integration/install.sh
@@ -6,7 +6,7 @@ zypper --non-interactive ar http://download.opensuse.org/repositories/openSUSE:/
 zypper --non-interactive --gpg-auto-import-keys ref
 zypper --non-interactive up
 zypper --non-interactive in -t pattern devel_osc_build
-zypper --non-interactive install wget curl timezone \
+zypper --non-interactive install --no-recommends wget curl timezone \
   gcc-c++ libffi-devel git-core zlib-devel libxml2-devel libxslt-devel libmariadb-devel \
   mariadb-client mariadb ruby2.5-rubygem-bundler make build sudo ruby-devel nginx obs-service-format_spec_file
 SUSEConnect -d
