9fac6dc154c919f51338efca79cd6079258155a2
Harald <hmuelle@suse.de>
2021-05-03 12:49:47 +0200

    Update contrib/get_repository_tokens.py
    Co-authored-by: Natnael Getahun <natnael.getahun@suse.com>

--- contrib/get_repository_tokens.py
+++ contrib/get_repository_tokens.py
@@ -36,7 +36,7 @@ def print_USAGE():
     print 'Usage:'
     print('\t'+self+' -u <username> -p <password> <part of repository URL> [... <part of repository URL>]')
     print '\tUse the SCC mirror credentials of your organization for user and password'
-    print '\tProvide strings to match against repository URL'
+    print('\tProvide strings to match against repository URL')
     print '\tThe script provides all matches, one per line, full URL and TOKEN separated by ",".'
 
 def main(argv):
