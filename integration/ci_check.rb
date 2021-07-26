572c89ddca68c1553348ef7a13b9acebebce8fb5
ngetahun <natnael.getahun@suse.com>
2021-07-26 17:27:37 +0200

    - correct fetch arguments
    - Add logging
    
    - Use proper master ref
    
    - Use correct directory
    
    - It's working

--- integration/ci_check.rb
+++ integration/ci_check.rb
@@ -1,29 +1,14 @@
 #! /usr/bin/env ruby
 
-def work_dir(path, &block)
-  return nil unless block_given?
-
-  old_dir = Dir.pwd
-  Dir.chdir path
-  yield block
-  Dir.chdir old_dir
-end
-
 def modified_files
-  files = ''
-  work_dir '../' do
-    puts "Fetching master... #{Dir.pwd}"
-    `git fetch origin master`
-    puts "Computing diff with master #{Dir.pwd}"
-    files = `git diff --name-only master`.strip.split "\n"
-  end
-  files
+  `git fetch origin master`
+  `git diff --name-only origin/master`.strip.split "\n"
 end
 
 def spec_version
   return @_spec_version if defined?(@_spec_version)
 
-  @_spec_version = File.open('../package/obs/rmt-server.spec', 'r') do |f|
+  @_spec_version = File.open('package/obs/rmt-server.spec', 'r') do |f|
     f.each_line do |line|
       break line.split(':').last.strip if /^Version/.match?(line)
     end
@@ -37,24 +22,17 @@ def rmt_version
   @_rmt_version = RMT::VERSION
 end
 
-def fail(msg)
-  `echo ::error ""`
-  `echo ::set-output name=error::msg::msg ""`
-  `echo ::set-output name=error::msg::msg "#{msg}"`
+def failure(msg)
+  warning(msg)
   exit 1
 end
 
 def success
-  `echo ::success ""`
-  `echo ::set-output name=success::msg::msg ""`
-  `echo ::set-output name=success::msg::msg "Checks passed."`
-  exit 0
+  puts 'Checks passed.'
 end
 
 def warning(msg)
-  `echo ::warning ""`
-  `echo ::set-output name=warning::msg::msg ""`
-  `echo ::set-output name=warning::msg::msg "#{msg}"`
+  puts msg.to_s
 end
 
 def check
@@ -68,9 +46,10 @@ def check
     error_msg = "The version of RMT is not consistent. These files must specify the same version:\n"
     error_msg << "- `lib/rmt.rb` specifies `#{rmt_version}`\n"
     error_msg << "- `package/obs/rmt-server.spec` specifies `#{spec_version}`\n"
-    fail(error_msg)
+    failure(error_msg)
   end
   success
+  exit 0
 end
 
 check
