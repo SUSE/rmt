7495917e8c595a341a7ff518aa15531c06c4a9a2
ngetahun <natnael.getahun@suse.com>
2021-09-28 09:23:06 +0200

    Load global config if readable by current process uid.

--- lib/rmt/config.rb
+++ lib/rmt/config.rb
@@ -6,15 +6,11 @@ Config.setup do |config|
 end
 
 Config.load_and_set_settings(
+  ('/etc/rmt.conf' if File.readable?('/etc/rmt.conf')),
   File.join(__dir__, '../../config/rmt.yml'),
   File.join(__dir__, '../../config/rmt.local.yml')
 )
 
-if ENV.fetch('RMT_ENV', 'development') == 'production'
-  Settings.reload_from_files(
-    '/etc/rmt.conf'
-  )
-end
 
 module RMT::Config
   class << self
