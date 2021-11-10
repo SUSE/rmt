8094bfb044063ca9945d6e2aab45ad5277a648e2
Luis Caparroz <lcaparroz@suse.com>
2021-11-10 14:15:36 -0300

    Add file age checking for clean packages command
    The zypper service relies on cached metadata, with a TTL of 1 day.
    During this period, the command should keep files to avoid problems with
    systems registered to an RMT proxy trying to install mirrored packages.

--- spec/support/shared_examples/cli/clean.rb
+++ spec/support/shared_examples/cli/clean.rb
@@ -24,8 +24,8 @@ shared_examples 'does not remove database entries' do
   end
 end
 
-shared_examples 'remove files' do
-  it 'remove all stale files' do
+shared_examples 'removes files' do
+  it 'removes all stale files' do
     silence_stdout do
       expect { command }
         .to change { Dir.glob(File.join(mirror_dir, '**', '*')).count }
@@ -34,8 +34,8 @@ shared_examples 'remove files' do
   end
 end
 
-shared_examples 'remove source files' do
-  it 'remove source files' do
+shared_examples 'removes source files' do
+  it 'removes source files' do
     silence_stdout do
       expect { command }
         .to change { Dir.glob(File.join(mirror_dir, '**', '*.src.rpm')).count }
@@ -44,12 +44,35 @@ shared_examples 'remove source files' do
   end
 end
 
-shared_examples 'remove database entries' do
-  it 'remove all database entries referring to stale files' do
+shared_examples 'removes database entries' do
+  it 'removes all database entries referencing stale files' do
     silence_stdout do
       expect { command }
         .to change(DownloadedFile, :count)
         .by(-stale_database_entries.count)
+    end
+  end
+end
+
+shared_examples 'does not remove fresh stale files' do
+  it 'does not remove fresh stale files' do
+    silence_stdout do
+      # File.stat will fail if the file doesn't exist, which come in hand in
+      # case the implementation fails to keep the files.
+      expect { command }.not_to change {
+        fresh_stale_files.map { |f| File.stat(f[:file]).inspect }
+      }
+    end
+  end
+end
+
+shared_examples 'does not remove database entries of fresh stale files' do
+  it 'does not remove database entries referencing fresh stale files' do
+    fresh_files = fresh_stale_database_entries.pluck(:file)
+    silence_stdout do
+      expect { command }.not_to change {
+        DownloadedFile.where(local_path: fresh_files).pluck(:local_path)
+      }.from(fresh_files)
     end
   end
 end
