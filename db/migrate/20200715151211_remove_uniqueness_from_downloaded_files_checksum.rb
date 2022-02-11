fe8c47e947d4ab5d56ba606c0d9046220991ca38
Thomas Schmidt <tschmidt@suse.de>
2022-02-11 17:43:05 +0100

    Drop indexes in the migration only if they exist. This shouldn't be needed in theory, but we had customers running into this issue, maybe they manually dropped those indexes before.

--- db/migrate/20200715151211_remove_uniqueness_from_downloaded_files_checksum.rb
+++ db/migrate/20200715151211_remove_uniqueness_from_downloaded_files_checksum.rb
@@ -1,6 +1,6 @@
 class RemoveUniquenessFromDownloadedFilesChecksum < ActiveRecord::Migration[5.2]
   def change
-    remove_index :downloaded_files, name: :index_downloaded_files_on_checksum_type_and_checksum
+    remove_index :downloaded_files, name: :index_downloaded_files_on_checksum_type_and_checksum, if_exists: true
     add_index :downloaded_files, %i[checksum_type checksum]
   end
 end
