57f05ee23d757ccf4ef6259fa627c86d3462ed38
ngetahun <natnael.getahun@suse.com>
2022-05-10 15:09:17 +0200

    Remove ruby warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  $VERBOSE = nil
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
