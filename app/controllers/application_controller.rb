8b1794885ba76229a5ecc6e5628a224b790c466d
Luis Caparroz <lcaparroz@suse.com>
2022-07-12 19:25:39 +0200

    Add system token validation to authentication
    This commit implements the validation of the 'System-Token' header on
    requests that require system authentication.
    
    The validation of `System-Token` is the base for uniquely reporting
    system, and it implies duplicating systems whenever the validation
    fails.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  SYSTEM_TOKEN_HEADER = 'System-Token'.freeze
 
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
@@ -8,16 +9,71 @@ class ApplicationController < ActionController::API
 
   def authenticate_system
     authenticate_or_request_with_http_basic('RMT API') do |login, password|
-      @system = System.find_by(login: login, password: password)
-      if @system
-        logger.info _('Authenticated system with login \"%{login}\"') % { login: login }
-        @system.touch(:last_seen_at)
+      @systems = System.get_by_credentials(login, password)
+      if @systems.present?
+        @system = find_system_by_token_header(@systems)
+
+        # If SYSTEM_TOKEN_HEADER is present, RMT assumes the client uses a SUSEConnect version
+        # that supports this feature. In this case, refresh the token and include it in the response.
+        if request.headers.key?(SYSTEM_TOKEN_HEADER)
+          @system.update(last_seen_at: Time.zone.now, system_token: SecureRandom.uuid)
+          headers[SYSTEM_TOKEN_HEADER] = @system.system_token
+        # only update last_seen_at each 3 minutes,
+        # so that a system that calls SCC every second doesn't write + lock the database row
+        elsif !@system.last_seen_at || @system.last_seen_at < 3.minutes.ago
+          @system.touch(:last_seen_at)
+        end
+        true
       else
-        logger.info _('Could not find system with login \"%{login}\" and password \"%{password}\"') % { login: login, password: password }
+        logger.info _('Could not find system with login \"%{login}\" and password \"%{password}\"') %
+          { login: login, password: password }
         error = ActionController::TranslatedError.new(N_('Invalid system credentials'))
         error.status = :unauthorized
         raise error
       end
+    end
+  end
+
+  private
+
+  # Token mechanism to detect duplicated systems.
+  # 1: system doesn't send a token header (old SUSEConnect version)
+  # 2: system sends a token, and it matches an existing system with that token
+  # 3: system sends an empty token, it matches an existing system without token
+  #    -> it's a new system (because the 'announce_system' call doesn't set the token yet)
+  #       or a system that upgraded to a SUSEConnect version that supports tokens
+  # 4: system sends an empty token, but a system with credentials and token exists (duplicate)
+  # 5: system sends a token, it matches an existing system but mismatch the token (duplicate)
+
+  def find_system_by_token_header(systems)
+    return nil if systems.blank?
+
+    # 1st case
+    unless request.headers.key?(SYSTEM_TOKEN_HEADER)
+      logger.info _('System with login \"%{login}\" authenticated without token header') %
+        { login: systems.first.login }
+      return systems.first
+    end
+
+    # 2nd/3rd case
+    system_token = request.headers[SYSTEM_TOKEN_HEADER].presence
+    system = systems.find { |s| s.system_token == system_token }
+    if system
+      logger.info _('System with login \"%{login}\" authenticated with token \"%{system_token}\"') %
+        { login: system.login, system_token: system_token }
+      return system
+    end
+
+    # 4th/5th case
+    system = systems.first
+    system.dup.tap do |ns|
+      ns.created_at = Time.zone.now
+      ns.system_token = system_token
+      ns.activations = system.activations.map(&:dup)
+      ns.hw_info = system.hw_info.dup
+      ns.save!
+      logger.info _('System with login \"%{login}\" (ID %{new_id}) authenticated and duplicated from ID %{base_id} due to token mismatch') %
+        { login: ns.login, new_id: ns.id, base_id: system.id }
     end
   end
 end
---
+++ spec/requests/api/connect/base_controller_spec.rb
@@ -0,0 +1,180 @@
+require 'rails_helper'
+
+RSpec.describe Api::Connect::BaseController, type: :controller do
+  let(:subscription) { FactoryBot.create(:subscription) }
+  let(:system) { FactoryBot.create(:system) }
+
+  # Anonymous controller to test Api::Connect::BaseController
+  controller do
+    before_action :authenticate_system, only: :service
+    before_action :authenticate_with_token, only: :announce_system
+
+    def service
+      head :not_found
+    end
+
+    def announce_system
+      head :created
+    end
+
+    def activate
+    end
+
+    private
+
+    def require_product
+      require_params(%i[identifier version arch])
+    end
+  end
+
+  before do
+    @routes.draw do
+      get 'service' => 'api/connect/base#service'
+      post 'announce_system' => 'api/connect/base#announce_system'
+      post 'activate' => 'api/connect/base#activate'
+    end
+  end
+
+  describe '#authenticate_system' do
+    context 'with invalid credentials' do
+      it 'authentication fails with wrong credentials' do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(system.login, '')
+
+        get :service, params: { id: 1 }
+
+        expect(response.media_type).to eq 'application/json'
+        expect(response.status).to eq 401
+        expect(json_response[:error]).to eq 'Invalid system credentials'
+      end
+    end
+
+    context 'with valid credentials' do
+      before { request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(system.login, system.password) }
+
+      it 'authenticates the system with HTTP Basic Authentication' do
+        get :service, params: { id: 1 }
+        expect(response.status).to eq 404
+      end
+
+      it 'updates last_seen_at' do
+        time = Time.zone.now.round(0) # rounding to ms to match mysql precision
+        system.update(last_seen_at: 1.hour.ago)
+        Timecop.freeze(time) do
+          get :service, params: { id: 1 }
+
+          expect(system.reload.last_seen_at).to eq Time.zone.now
+        end
+      end
+
+      it 'sets last_seen_at if not yet set' do
+        time = Time.zone.now.round(0) # rounding to ms to match mysql precision
+        system.update(last_seen_at: nil)
+        Timecop.freeze(time) do
+          get :service, params: { id: 1 }
+
+          expect(system.reload.last_seen_at).to eq Time.zone.now
+        end
+      end
+
+      it "doesn't update last_seen_at if updated in the last 3 minutes" do
+        time = 1.minute.ago.round(0) # rounding to ms to match mysql precision
+        system.update(last_seen_at: time)
+        get :service, params: { id: 1 }
+        expect(system.reload.last_seen_at).to eq time
+      end
+    end
+
+    context 'system token' do
+      before do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic
+          .encode_credentials(system.login, system.password)
+      end
+
+      let(:new_system_token) { 'BBBBBBBB-BBBB-4BBB-9BBB-BBBBBBBBBBBB' }
+
+      context 'when the system does not have a token and the header is not present' do
+        let!(:system) { create(:system, hostname: 'system') }
+
+        include_examples 'does not create a duplicate system'
+        include_examples "does not update the old system's token"
+        include_examples 'does not respond with a token'
+      end
+
+      context 'when the system does not have a token and the header is blank' do
+        before { request.headers['System-Token'] = '' }
+
+        let(:current_system_token) { nil }
+        let!(:system) { create(:system, hostname: 'system') }
+
+        include_examples 'does not create a duplicate system'
+        include_examples 'updates the system token'
+        include_examples 'responds with a new token'
+      end
+
+      context 'when the system has a token and the header matches it' do
+        before { request.headers['System-Token'] = current_system_token }
+
+        let(:current_system_token) { 'AAAAAAAA-AAAA-4AAA-9AAA-AAAAAAAAAAAA' }
+        let!(:system) { create(:system, hostname: 'system', system_token: current_system_token) }
+
+        include_examples 'does not create a duplicate system'
+        include_examples 'updates the system token'
+        include_examples 'responds with a new token'
+      end
+
+      context 'when the system has a token and the header is blank' do
+        before { request.headers['System-Token'] = '' }
+
+        let(:current_system_token) { 'AAAAAAAA-AAAA-4AAA-9AAA-AAAAAAAAAAAA' }
+        let!(:system) do
+          create(:system, :with_activated_product, hostname: 'system',
+                 system_token: current_system_token)
+        end
+
+        include_examples "does not update the old system's token"
+        include_examples 'creates a duplicate system'
+        include_examples 'responds with a new token'
+      end
+
+      context 'when the system has a token and the header does not match it' do
+        before { request.headers['System-Token'] = wrong_system_token }
+
+        let(:current_system_token) { 'AAAAAAAA-AAAA-4AAA-9AAA-AAAAAAAAAAAA' }
+        let(:wrong_system_token)   { 'CCCCCCCC-CCCC-4CCC-9CCC-CCCCCCCCCCCC' }
+        let!(:system) do
+          create(:system, :with_activated_product, hostname: 'system',
+                 system_token: current_system_token)
+        end
+
+        include_examples "does not update the old system's token"
+        include_examples 'creates a duplicate system'
+        include_examples 'responds with a new token'
+      end
+    end
+  end
+
+  describe 'HTTP Token Authentication' do
+    context 'with invalid credentials' do
+      it 'authentication fails with wrong token' do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials('token')
+
+        post :announce_system, params: { format: :json }
+
+        expect(response.media_type).to eq 'application/json'
+        expect(response.status).to eq 401
+        expect(json_response[:error]).to eq 'Unknown Registration Code.'
+      end
+    end
+
+    context 'with valid credentials' do
+      it 'authenticates the system with valid token' do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(subscription.regcode)
+
+        post :announce_system, params: { email: 'glue-dummy@mail.com'.to_json, format: :json }
+
+        expect(response.media_type).to eq 'application/json'
+        expect(response.status).to eq 201
+      end
+    end
+  end
+end
