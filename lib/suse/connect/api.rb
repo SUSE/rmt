43aa52bb7120be52be80308ba09bf0fe55748801
ngetahun <natnael.getahun@suse.com>
2022-09-09 10:58:59 +0200

    Remove redundant rescue block

--- lib/suse/connect/api.rb
+++ lib/suse/connect/api.rb
@@ -80,11 +80,6 @@ module SUSE
           system_limit = e.response.headers['X-Payload-Entities-Max-Limit'].to_i
           send_bulk_system_update(systems, system_limit)
         end
-      # rubocop:disable Lint/RescueException
-      rescue Exception => e
-        # rubocop:enable Lint/RescueException
-        @logger.error(e)
-        raise e
       # :nocov:
       else
         updated_systems
