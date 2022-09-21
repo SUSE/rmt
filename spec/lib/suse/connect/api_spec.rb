325b20695dd270c5845bc14dcb77c418936e21d9
Felix Schnizlein <felix@schnizle.in>
2022-09-21 16:18:28 +0200

    Use Systemserializer in api_spec.rb

--- spec/lib/suse/connect/api_spec.rb
+++ spec/lib/suse/connect/api_spec.rb
@@ -216,17 +216,15 @@ RSpec.describe SUSE::Connect::Api do
         }
       end
 
-      let(:systems) { [] }
-
-      let(:system_keys) { %i[id login password last_seen_at] }
-      let(:product_keys) { %i[id identifier version arch] }
-      let(:hwinfo_keys) { %i[cpus sockets hypervisor arch uuid cloud_provider] }
-
-      let(:expected_body) { { systems: [] } }
+      let(:expected_body) do
+        { systems: systems.map { |s| SUSE::Connect::SystemSerializer.new(s) } }
+      end
 
       let(:expected_response) do
+        keys = %i[id login password last_seen_at]
+
         system_hashes = systems.collect do |s|
-          s.slice(*system_keys).symbolize_keys.transform_values!(&:to_s)
+          s.slice(*keys).symbolize_keys.transform_values!(&:to_s)
         end
         { systems: system_hashes }
       end
@@ -248,29 +246,6 @@ RSpec.describe SUSE::Connect::Api do
 
       context 'when a single system is bulk updated' do
         let(:systems) { create_list :system, 1, :full }
-        let(:activation) { system.activations.first }
-        let(:product) { activation.product }
-        let(:hw_info) { system.hw_info }
-
-        let(:expected_body) do
-          system_hashes = systems.collect do |system|
-            activation = system.activations.first
-            product = activation.product.slice(*product_keys)
-            product[:activated_at] = activation.created_at
-
-            hwinfo = system.hw_info.slice(*hwinfo_keys)
-            {
-              login: system.login,
-              password: system.password,
-              last_seen_at: system.last_seen_at,
-              created_at: system.created_at,
-              hostname: system.hostname,
-              hwinfo: hwinfo,
-              products: [product]
-            }
-          end.compact
-          { systems: system_hashes }
-        end
 
         it 'yields results' do
           expect(api_client.send_bulk_system_update(relation)).to eq(expected_response)
@@ -279,27 +254,6 @@ RSpec.describe SUSE::Connect::Api do
 
       context 'when sending in bulk' do
         let(:systems) { create_list :system, 3, :full, scc_synced_at: nil }
-        let(:product) { system.products.first }
-        let(:hw_info) { system.hw_info }
-
-        let(:expected_body) do
-          system_hashes = systems.collect do |system|
-            activation = system.activations.first
-            product = activation.product.slice(*product_keys)
-            product[:activated_at] = activation.created_at
-            hwinfo = system.hw_info.slice(*hwinfo_keys)
-            {
-              login: system.login,
-              password: system.password,
-              last_seen_at: system.last_seen_at,
-              created_at: system.created_at,
-              hostname: system.hostname,
-              hwinfo: hwinfo,
-              products: [product]
-            }
-          end.compact
-          { systems: system_hashes }
-        end
 
         it 'yields successful results' do
           expect(api_client.send_bulk_system_update(relation)).to eq(expected_response)
@@ -307,30 +261,9 @@ RSpec.describe SUSE::Connect::Api do
       end
 
       context 'when sending in bulk with and without system_token' do
-        let!(:system1) { create :system, :full, :with_system_token, :synced }
-        let!(:system2) { create :system, :full, :synced }
-
+        let!(:system1) { create :system, :full, :with_system_token }
+        let!(:system2) { create :system, :full }
         let(:systems) { [system1, system2] }
-
-        let(:expected_body) do
-          {
-            systems: [
-              {
-                login: system1.login,
-                password: system1.password,
-                last_seen_at: system1.last_seen_at,
-                created_at: system1.created_at,
-                system_token: system1.id
-              },
-              {
-                login: system2.login,
-                password: system2.password,
-                last_seen_at: system2.last_seen_at,
-                created_at: system2.created_at
-              }
-            ]
-          }
-        end
 
         it 'yields successful results' do
           expect(api_client.send_bulk_system_update(relation)).to eq(expected_response)
@@ -338,108 +271,49 @@ RSpec.describe SUSE::Connect::Api do
       end
 
       context 'when sending in bulk and encounter 413 http error code' do
+        let(:systems) { create_list(:system, 2, :full) }
+        # When we get a 413, the logic will restart and send each system
+        # one by one since the limit is set to 1
+        let!(:stubbed) do
+          expected_body[:systems].each_with_index.map do |payload, i|
+            stub_request(:put, 'https://scc.suse.com/connect/organizations/systems')
+              .with(
+                headers: expected_request_headers,
+                body: { systems: [payload] }.to_json
+              )
+              .to_return(
+                status: 201,
+                body: { systems: [expected_response[:systems][i]] }.to_json
+              )
+          end
+        end
+
         before do
+          # Stub the initial request and return 413
           stub_request(:put, 'https://scc.suse.com/connect/organizations/systems')
             .with(
               headers: expected_request_headers,
-              body: all_systems_payload.to_json
+              body: expected_body.to_json
             )
             .to_return(
               status: 413,
-              headers: {
-                'X-Payload-Entities-Max-Limit': 1
-              },
+              headers: { 'X-Payload-Entities-Max-Limit': 1 },
               body: [].to_json
             )
-          stub_request(:put, 'https://scc.suse.com/connect/organizations/systems')
-            .with(
-              headers: expected_request_headers
-            )
-            .to_return(
-              status: 201,
-              body: expected_response.to_json,
-              headers: {}
-            )
         end
 
-        let(:subscription) { create :subscription }
-        let(:systems) { create_list(:system, 2, :full, subscription: subscription, scc_synced_at: nil) }
-
-        let(:expected_response) { { systems: [systems.last.slice(*system_keys).symbolize_keys.transform_values!(&:to_s)] } }
-
-        let(:all_systems_payload) do
-          system_hashes = systems.collect do |system|
-            product = system.products.first.slice(*product_keys)
-            product[:regcode] = subscription.regcode
-
-            hwinfo = system.hw_info.slice(*hwinfo_keys)
-            {
-              login: system.login,
-              password: system.password,
-              last_seen_at: system.last_seen_at,
-              hostname: system.hostname,
-              hwinfo: hwinfo,
-              products: [product]
-            }.transform_values!(&:to_s)
-          end.compact
-          { systems: system_hashes }
-        end
-
-        let(:expected_body) do
-          system_hashes = systems.take(1).collect do |system|
-            product = system.products.first.slice(*product_keys)
-            product[:regcode] = subscription.regcode
-
-            hwinfo = system.hw_info.slice(*hwinfo_keys)
-            {
-              login: system.login,
-              password: system.password,
-              last_seen_at: system.last_seen_at,
-              hostname: system.hostname,
-              hwinfo: hwinfo,
-              products: [product]
-            }
-          end.compact
-          { systems: system_hashes }
-        end
 
         it 'yields successful results' do
           expect(api_client.send_bulk_system_update(relation)).to eq(expected_response)
+
+          expect(stubbed).to all(have_been_requested)
         end
       end
 
       context 'when sending in bulk and a system which only sends last_seen_at' do
-        let(:system_set) { create :system, :synced }
+        let(:system_set) { create :system, :full, :synced }
         let(:systems_unset) { create_list :system, 2, :full }
-
         let(:systems) { [system_set] + systems_unset }
-
-        let(:expected_body_set) do
-          {
-            login: system_set.login,
-            password: system_set.password,
-            last_seen_at: system_set.last_seen_at,
-            created_at: system_set.created_at
-          }
-        end
-        let(:expected_body_unset) do
-          systems_unset.collect do |system|
-            activation = system.activations.first
-            product = activation.product.slice(*product_keys)
-            product[:activated_at] = activation.created_at
-            hwinfo = system.hw_info.slice(*hwinfo_keys)
-            {
-              login: system.login,
-              password: system.password,
-              last_seen_at: system.last_seen_at,
-              created_at: system.created_at,
-              hostname: system.hostname,
-              hwinfo: hwinfo,
-              products: [product]
-            }
-          end
-        end
-        let(:expected_body) { { systems: [expected_body_set] + expected_body_unset } }
 
         it 'yields successful results' do
           expect(api_client.send_bulk_system_update(relation)).to eq(expected_response)
