53e81ec27bb2f6292792be99c9272cfe95207430
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-04-25 10:03:34 +0100

    Restore test

--- engines/strict_authentication/spec/requests/strict_authentication/authentication_controller_spec.rb
+++ engines/strict_authentication/spec/requests/strict_authentication/authentication_controller_spec.rb
@@ -100,6 +100,28 @@ module StrictAuthentication
             end
           end
 
+          context 'when accessing SLES12SP1 repos and SLES 11 is not activated' do
+            let(:system) { FactoryBot.create(:system, :with_activated_product, product: product) }
+            let(:product) do
+              FactoryBot.create(
+                :product, :with_mirrored_repositories,
+                identifier: 'SLES', version: '15', arch: 'x86_64'
+              )
+            end
+
+            context 'when requested path is version 12' do
+              let(:requested_uri) { '/repo/SUSE/Updates/SLE-Module-Adv-Systems-Management/12/x86_64/update' }
+
+              its(:code) { is_expected.to eq '403' }
+            end
+
+            context 'when requested path is version 12.1' do
+              let(:requested_uri) { '/repo/SUSE/Products/SLE-SERVER/12-SP1/x86_64/product' }
+
+              its(:code) { is_expected.to eq '403' }
+            end
+          end
+
           context 'when system is SUMA' do
             let(:my_product) do
               FactoryBot.create(
