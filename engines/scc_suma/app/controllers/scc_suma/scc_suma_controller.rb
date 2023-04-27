802de29d280291c39832ed443431939c9389cbd7
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-04-27 17:03:05 +0100

    Add products-unscoped

--- engines/scc_suma/app/controllers/scc_suma/scc_suma_controller.rb
+++ engines/scc_suma/app/controllers/scc_suma/scc_suma_controller.rb
@@ -1,9 +1,10 @@
 module SccSuma
   class SccSumaController < ::ApplicationController
-    before_action :is_valid?, only: %w[unscoped_products get_list]
+    before_action :is_valid?, only: %w[get_scc_client]
+    before_action :get_scc_client, only: %w[unscoped_products get_list]
 
     def unscoped_products
-      render status: :ok, json: { result: 'some_result' }
+      render status: :ok, json: { result: @scc_api_client.list_products_unscoped.to_json }
     end
 
     def get_list
@@ -12,6 +13,13 @@ module SccSuma
 
     def product_tree
       render status: :ok, json: { result: 'some_result' }
+    end
+
+    def get_scc_client
+      @scc_api_client = SUSE::Connect::Api.new(
+        Settings.scc.username,
+        Settings.scc.password
+      )
     end
 
     protected
