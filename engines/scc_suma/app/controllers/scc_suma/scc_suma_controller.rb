9bdd33ca3ad8883d61685aaac74f120c21478cc4
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-04-27 20:39:41 +0100

    Download product_tree.json from SCC

--- engines/scc_suma/app/controllers/scc_suma/scc_suma_controller.rb
+++ engines/scc_suma/app/controllers/scc_suma/scc_suma_controller.rb
@@ -1,4 +1,8 @@
+require 'json'
+
 module SccSuma
+  REPOSITORY_URL = 'https://scc.suse.com/suma/'.freeze
+
   class SccSumaController < ::ApplicationController
     before_action :is_valid?, only: %w[get_scc_client]
     before_action :get_scc_client, only: %w[unscoped_products get_list]
@@ -12,7 +16,8 @@ module SccSuma
     end
 
     def product_tree
-      render status: :ok, json: { result: 'some_result' }
+      json_product_tree = get_product_tree_json
+      render status: :ok, json: { result: json_product_tree }
     end
 
     def get_scc_client
@@ -32,6 +37,25 @@ module SccSuma
         params[:metadata]
       )
       raise 'Unspecified error' unless verification_provider.instance_valid?
+    end
+
+    def get_product_tree_json
+      download_scc_file
+      JSON.File.open(@product_tree_file.local_path).read
+    end
+
+    def download_scc_file
+      tmp_dir = Rails.root.join('tmp')
+      downloading_paths = {
+        base_url: URI.join(REPOSITORY_URL),
+        base_dir: tmp_dir,
+        cache_dir: tmp_dir
+      }
+
+      @product_tree_file = RMT::Mirror::FileReference.new(relative_path: 'product_tree.json', **downloading_paths)
+      downloader = RMT::Downloader.new(logger: logger, track_files: false)
+      logger.info _('Downloading SUSE Manager product tree to %{dir}') % { dir: tmp_dir }
+      downloader.download_multi([@product_tree_file])
     end
   end
 end
