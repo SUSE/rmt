454537ec0e2225bcaf288f3c0a17de9147cf67b5
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-07-14 15:58:21 +0100

    Add debug option
    Adding a debug flag/option in order to
    be able to skip some verifications when
    using rmt-manual-instance-verify when
    debugging certain issues
    
    This Fixes bsc#1210160

--- engines/instance_verification/lib/instance_verification/provider_base.rb
+++ engines/instance_verification/lib/instance_verification/provider_base.rb
@@ -3,13 +3,14 @@ class InstanceVerification::ProviderBase
     InstanceVerification.provider = child_class
   end
 
-  def initialize(logger, request, product_hash, instance_data)
+  def initialize(logger, request, product_hash, instance_data, debug = false)
     @logger = logger
     @request = request
     @product_hash = product_hash
     @instance_data = instance_data
     @instance_id = nil # set by CSP-specific implementation, used for logging errors
     @instance_billing_info = nil # set by CSP-specific implementation, used for logging errors
+    @debug = debug
   end
 
   attr_reader(:instance_id, :instance_billing_info)
