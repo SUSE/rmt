f1a3be998e20c09fad55ae2ce28b99d60699f68b
Felix Schizlein <fschnizlein@suse.com>
2023-07-18 11:46:35 +0200

    Do not set the default value directly to circumvent table locking

--- db/migrate/20230717143633_add_system_information_to_systems.rb
+++ db/migrate/20230717143633_add_system_information_to_systems.rb
@@ -1,5 +1,5 @@
 class AddSystemInformationToSystems < ActiveRecord::Migration[6.1]
-  def change
+  def up
     # MySQL / MariaDB use the `json` data type as alias for longtext
     # in case you are wondering why schema.rb shows longtext.
     # See here: https://mariadb.com/kb/en/json-data-type/
@@ -8,9 +8,15 @@ class AddSystemInformationToSystems < ActiveRecord::Migration[6.1]
     # is actually valid JSON
     # Something along the line:
     # add_check_constraint :systems, 'json_valid(`system_information`)', name: 'system_information'
-    #
     # check schema.rb to see the result
-    add_column :systems, :system_information, :json, default: {}
+    #
+    # NOTE: JSON/TEXT columns do not allow to set a default value!
+    add_column :systems, :system_information, :json
     add_column :systems, :instance_data, :string
+  end
+
+  def down
+    remove_column :systems, :system_information
+    remove_column :systems, :instance_data
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -162,7 +162,7 @@ ActiveRecord::Schema.define(version: 2022_07_11_152732) do
     t.bigint "scc_system_id", comment: "System ID in SCC (if the system registration was forwarded; needed for forwarding de-registrations)"
     t.boolean "proxy_byos", default: false
     t.string "system_token"
-    t.text "system_information", size: :long, default: "{}", collation: "utf8"
+    t.text "system_information", size: :long
     t.string "instance_data"
     t.index ["login", "password", "system_token"], name: "index_systems_on_login_and_password_and_system_token", unique: true
     t.index ["login", "password"], name: "index_systems_on_login_and_password"
