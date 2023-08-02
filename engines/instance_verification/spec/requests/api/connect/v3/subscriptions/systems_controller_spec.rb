98f397c38fe97b53d78415abd878ce0269b3de4e
Felix Schizlein <fschnizlein@suse.com>
2023-08-02 11:33:59 +0200

    Make instance_verification engine aware of system_information column in systems and use the column accordingly

--- engines/instance_verification/spec/requests/api/connect/v3/subscriptions/systems_controller_spec.rb
+++ engines/instance_verification/spec/requests/api/connect/v3/subscriptions/systems_controller_spec.rb
@@ -9,7 +9,7 @@ describe Api::Connect::V3::Subscriptions::SystemsController, type: :request do
         post '/connect/subscriptions/systems', params: { hostname: 'test', instance_data: instance_data }
         data = JSON.parse(response.body)
         system = System.find_by(login: data['login'])
-        expect(system.hw_info.instance_data).to eq(instance_data)
+        expect(system.instance_data).to eq(instance_data)
       end
     end
   end
