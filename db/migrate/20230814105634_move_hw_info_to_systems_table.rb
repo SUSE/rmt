e0e54e91d6e3fcdc79b8d4945518a124483eae60
Miquel Sabaté Solà <msabate@suse.com>
2023-08-21 10:02:37 +0200

    Fixed issues reported by strong_migrations
    These are old migrations that should be considered already safe since we
    have applied them since years ago. Given that we are moving
    `strong_migrations` to the production environment (where `prophet` is
    applied), some issues are now being reported for the first time.
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- db/migrate/20230814105634_move_hw_info_to_systems_table.rb
+++ db/migrate/20230814105634_move_hw_info_to_systems_table.rb
@@ -1,7 +1,7 @@
 class MoveHwInfoToSystemsTable < ActiveRecord::Migration[6.1]
   def up
-
-    execute "update systems as s inner join hw_infos hw on s.id=hw.system_id \
+    safety_assured do
+      execute "update systems as s inner join hw_infos hw on s.id=hw.system_id \
                 set system_information = json_object(\
                 'cpus', hw.cpus, \
                 'sockets', hw.sockets, \
@@ -9,9 +9,12 @@ class MoveHwInfoToSystemsTable < ActiveRecord::Migration[6.1]
                 'arch', nullif(hw.arch, ''), \
                 'uuid', nullif(hw.uuid, ''), \
                 'cloud_provider', nullif(hw.cloud_provider, ''));"
+    end
   end
 
   def down
-    execute 'update systems set system_information = json_object();'
+    safety_assured do
+      execute 'update systems set system_information = json_object();'
+    end
   end
 end
