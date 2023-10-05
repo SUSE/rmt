e275055236bd5948d74de71d02a6c1fefda40f8d
Natnael Getahun <natnael.getahun@suse.com>
2023-10-05 12:09:35 +0200

    Increase lock timeout limit to silence stong migration warnings

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,5 +1,6 @@
 # rubocop:disable Style/NumericLiterals
 unless Rails.env.production?
   StrongMigrations.start_after = 20200205123840
+  StrongMigrations.lock_timeout_limit = 86400.seconds
 end
 # rubocop:enable Style/NumericLiterals
