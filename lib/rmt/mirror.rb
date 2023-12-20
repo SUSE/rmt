6b6401218408b720a46599bdbf42456d87ea6c30
Felix Schizlein <fschnizlein@suse.com>
2023-12-20 10:50:06 +0100

    Detect repository types when mirroring repositories

--- lib/rmt/mirror.rb
+++ lib/rmt/mirror.rb
@@ -13,5 +13,26 @@ class RMT::Mirror
   end
 
   def detect_repository_type
+    search = {
+      repomd: File.join(repository.external_url, RPM_FILE_NEEDLE),
+      debian: File.join(repository.external_url, DEB_FILE_NEEDLE)
+    }
+
+    search.each do |key, url|
+      # Current CDN authenticates via a key append to the request path
+      # e.g.
+      # https://update.suse.com/SUSE/product/some-product
+      # becomes
+      # https://update.suse.com/SUSE/product/some-product?authenication_tokensiduhashasdyashdaysdasud
+      uri = URI.join(url)
+      uri.query = @repository.auth_token if @repository.auth_token
+
+      request = RMT::HttpRequest.new(uri, method: :head, followlocation: true)
+      request.on_success do
+        return key
+      end
+      request.run
+    end
+    nil
   end
 end
