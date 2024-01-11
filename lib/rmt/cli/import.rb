7e38365a341982daad9cf25af518539b9aec9a7b
Felix Schizlein <fschnizlein@suse.com>
2024-01-11 09:47:21 +0100

    Cleanup tests in cli/mirror and started refreshing import/export

--- lib/rmt/cli/import.rb
+++ lib/rmt/cli/import.rb
@@ -12,16 +12,14 @@ class RMT::CLI::Import < RMT::CLI::Base
   def repos(path)
     RMT::Lockfile.lock do
       path = needs_path(path)
-
-      mirror = RMT::Mirror::Repomd.new(logger: logger, airgap_mode: true)
-
       repos_file = File.join(path, 'repos.json')
+
       raise RMT::CLI::Error.new(_('%{file} does not exist.') % { file: repos_file }) unless File.exist?(repos_file)
 
       begin
         exported_suma_path = File.join(path, '/suma/')
         suma_repo_url = URI.join('file://', exported_suma_path).to_s
-        suma_product_tree = RMT::Mirror::SumaProductTree.new(mirroring_base_dir: exported_suma_path, logger: logger, url: suma_repo_url)
+        suma_product_tree = RMT::Mirror::SumaProductTree.new(mirroring_base_dir: RMT::DEFAULT_MIRROR_DIR, logger: logger, url: suma_repo_url)
         suma_product_tree.mirror
       rescue RMT::Mirror::Exception => e
         logger.warn(e.message)
@@ -30,6 +28,7 @@ class RMT::CLI::Import < RMT::CLI::Base
       repos = JSON.parse(File.read(repos_file))
       repos.each do |repo_json|
         repo = Repository.find_by(external_url: repo_json['url'])
+
         if repo.nil?
           warn _('repository by URL %{url} does not exist in database') % { url: repo_json['url'] }
           next
@@ -38,12 +37,16 @@ class RMT::CLI::Import < RMT::CLI::Base
         begin
           exported_repo_path = File.join(path, Repository.make_local_path(repo_json['url']))
           repo_url = URI.join('file://', exported_repo_path).to_s
-          mirror.mirror(
-            repository_url: repo_url,
-            local_path: Repository.make_local_path(repo.external_url),
-            auth_token: repo.auth_token,
-            repo_name: repo.name
-          )
+          repo.external_url = repo_url
+
+          configuration = {
+            repository: repo,
+            logger: logger,
+            mirroring_base_dir: RMT::DEFAULT_MIRROR_DIR,
+            mirror_sources: RMT::Config.mirror_src_files?,
+            is_airgapped: true
+          }
+          RMT::Mirror.new(**configuration).mirror_now
 
           repo.refresh_timestamp!
         rescue RMT::Mirror::Exception => e
