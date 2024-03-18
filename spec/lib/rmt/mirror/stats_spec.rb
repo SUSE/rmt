a5319ab5a3d70a664a3aeb5b9984c18983066ed3
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-03-18 19:42:52 +0530

    rubocop fixes

--- spec/lib/rmt/mirror/stats_spec.rb
+++ spec/lib/rmt/mirror/stats_spec.rb
@@ -2,44 +2,44 @@ require 'rspec'
 require 'rails_helper'
 
 RSpec.describe RMT::Mirror::Stats do
-  subject { described_class.new }
+  subject(stats) { described_class.new }
 
   describe '#initialize' do
     context 'with default arguments' do
       it 'creates a new download_stats object of type RMT::Downloader::Stats' do
-        expect(subject.download_stats).to be_a(RMT::Downloader::Stats)
+        expect(stats.download_stats).to be_a(RMT::Downloader::Stats)
       end
     end
   end
 
   describe '#def_delegators' do
     it 'delegates total_size_in_mb to download_stats' do
-      expect(subject).to delegate_method(:total_size_in_mb).to(:download_stats)
+      expect(stats).to delegate_method(:total_size_in_mb).to(:download_stats)
     end
 
     it 'delegates files_count to download_stats' do
-      expect(subject).to delegate_method(:files_count).to(:download_stats)
+      expect(stats).to delegate_method(:files_count).to(:download_stats)
     end
   end
 
   describe '#reset!' do
     before do
-      subject.increment_mirrored_repos_count
+      stats.increment_mirrored_repos_count
     end
 
     it 'resets mirrored_repos_count to 0' do
-      expect { subject.reset! }.to change(subject, :mirrored_repos_count).to(0)
+      expect { stats.reset! }.to change(subject, :mirrored_repos_count).to(0)
     end
 
     it 'resets download_stats' do
-      expect(subject.download_stats).to receive(:reset!)
-      subject.reset!
+      expect(stats.download_stats).to receive(:reset!)
+      stats.reset!
     end
   end
 
   describe '#increment_mirrored_repos_count' do
     it 'increments mirrored_repos_count by 1' do
-      expect { subject.increment_mirrored_repos_count }.to change(subject, :mirrored_repos_count).by(1)
+      expect { stats.increment_mirrored_repos_count }.to change(subject, :mirrored_repos_count).by(1)
     end
   end
 
@@ -47,9 +47,9 @@ RSpec.describe RMT::Mirror::Stats do
     after { Timecop.return }
 
     it 'calculates the elapsed time since start_time rounded to seconds' do
-      subject.reset!
+      stats.reset!
       Timecop.travel 10.seconds.from_now
-      expect(subject.elapsed_seconds).to eq(10)
+      expect(stats.elapsed_seconds).to eq(10)
     end
   end
 end
