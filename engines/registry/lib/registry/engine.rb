a5c07de27e51a86f1318b51d03e18bf8f1eef3b1
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-04-19 13:18:25 +0100

    Rubocop fix
    Remove unnecessary comment

--- engines/registry/lib/registry/engine.rb
+++ engines/registry/lib/registry/engine.rb
@@ -5,17 +5,14 @@ module Registry
     isolate_namespace Registry
     config.generators.api_only = true
 
-    # REGISTRY_CACHE_PATH = Rails.root.join('tmp', 'registry', 'cache')
-
     config.after_initialize do
       Api::Connect::V3::Systems::ActivationsController.class_eval do
-
         before_action :handle_cache, only: %w[index]
 
         def handle_cache
           # get request header
           if request.headers['X-Registry']
-            registry_cache_path = Rails.root.join('tmp', 'registry', 'cache')
+            registry_cache_path = Rails.root.join('tmp/registry/cache')
             Dir.mkdir(registry_cache_path) unless Dir.exist?(registry_cache_path)
 
             cache_key = [request.remote_ip, @system.login].join('-')
