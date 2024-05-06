e7523dc3a927a312f728c4309a7ca521d1bacf1d
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-05-06 11:52:18 +0100

    Update registry access based on cache

--- engines/registry/spec/app/models/registry/authenticated_client_spec.rb
+++ engines/registry/spec/app/models/registry/authenticated_client_spec.rb
@@ -2,23 +2,37 @@ require 'spec_helper'
 
 describe Registry::AuthenticatedClient do
   describe '.new' do
-    context 'with system credentials' do
-      let(:system) { create(:system) }
+    let(:system) { create(:system) }
 
-      context 'with valid credentials' do
-        subject(:client) { described_class.new(system.login, system.password) }
+    context 'with invalid cache' do
+      before { allow_any_instance_of(described_class).to receive(:cache_file_exist?).and_return(false) }
 
-        it 'returns the auth strategy' do
-          expect(client.systems).to eq([system])
-          expect(client.auth_strategy).to eq(:system_credentials)
-        end
+      it 'raises an exception' do
+        expect { described_class.new(system.login, system.password, '127.0.0.1') }.to raise_error(
+          Registry::Exceptions::InvalidCredentials, /invalid cache/
+          )
       end
+    end
 
-      context 'with invalid password' do
-        subject(:client) { described_class.new(system.login, 'wrong') }
+    context 'with valid cache' do
+      context 'with system credentials' do
+        before { allow_any_instance_of(described_class).to receive(:cache_file_exist?).and_return(true) }
 
-        it 'raises' do
-          expect { client }.to raise_error(Registry::Exceptions::InvalidCredentials)
+        context 'with valid credentials' do
+          subject(:client) { described_class.new(system.login, system.password, '127.0.0.1') }
+
+          it 'returns the auth strategy' do
+            expect(client.systems).to eq([system])
+            expect(client.auth_strategy).to eq(:system_credentials)
+          end
+        end
+
+        context 'with invalid password' do
+          subject(:client) { described_class.new(system.login, 'wrong', '127.0.0.1') }
+
+          it 'raises' do
+            expect { client }.to raise_error(Registry::Exceptions::InvalidCredentials)
+          end
         end
       end
     end
