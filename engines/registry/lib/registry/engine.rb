e4adbea4fa58276f861d20da8774d4d663c0766c
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-05-06 12:45:10 +0100

    Delete registry cache when system is deregistered
    - Add before action method to remove the registry cache for the system
      when it gets deregistered

--- engines/registry/lib/registry/engine.rb
+++ engines/registry/lib/registry/engine.rb
@@ -1,4 +1,19 @@
 module Registry
+  class << self
+    def remove_cache(registry_cache_key)
+      cache_config_data = {}
+      File.open(Rails.application.config.cache_config_file, 'r') do |cache_config_file|
+        cache_config_file.each_line do |line|
+          line_data = line.split(/=|\n/)
+          cache_config_data[line_data[0]] = line_data[1]
+        end
+      end
+
+      registry_cache_path = File.join(cache_config_data['REGISTRY_CLIENT_CACHE_DIRECTORY'], registry_cache_key)
+      File.unlink(registry_cache_path) if File.exist?(registry_cache_path)
+    end
+  end
+
   class Engine < ::Rails::Engine
     isolate_namespace Registry
     config.generators.api_only = true
@@ -11,6 +26,15 @@ module Registry
           unless request.headers['X-Instance-Data'] && ZypperAuth.verify_instance(request, logger, @system, registry: true)
             render(xml: { error: 'Instance verification failed' }, status: :forbidden)
           end
+        end
+      end
+
+      Api::Connect::V3::Systems::SystemsController.class_eval do
+        before_action :remove_cache, only: %w[deregister]
+
+        def remove_cache
+          registry_cache_key = [request.remote_ip, @system.login].join('-')
+          Registry.remove_cache(registry_cache_key)
         end
       end
     end
