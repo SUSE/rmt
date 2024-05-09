c7a22cfec8898b21b451c43a77c4098c82e6a40e
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-05-09 17:22:37 +0100

    Use config variables

--- engines/registry/lib/registry/engine.rb
+++ engines/registry/lib/registry/engine.rb
@@ -1,16 +1,8 @@
 module Registry
   class << self
     def remove_auth_cache(registry_cache_key)
-      cache_config_data = {}
-      File.open(Rails.application.config.cache_config_file, 'r') do |cache_config_file|
-        cache_config_file.each_line do |line|
-          line_data = line.split(/=|\n/)
-          cache_config_data[line_data[0]] = line_data[1]
-        end
-      end
-
-      registry_cache_path = File.join(cache_config_data['REGISTRY_CLIENT_CACHE_DIRECTORY'], registry_cache_key)
-      File.unlink(registry_cache_path) if File.exist?(registry_cache_path)
+      cache_path = File.join(Rails.application.config.registry_cache_dir, registry_cache_key)
+      File.unlink(registry_cache_path) if File.exist?(cache_path)
     end
   end
 
