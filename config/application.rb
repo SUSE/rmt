7e30eddb135a11a034629aaee6727b04e5f4add8
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-05-15 10:39:14 +0100

    Use new cert for registry

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module RMT
       # registry config needed
       config.autoloader = :classic
       config.registry_private_key = OpenSSL::PKey::RSA.new(
-        File.read('/etc/rmt/ssl/rmt-server.key')
+        File.read('/etc/rmt/ssl/internal-registry.key')
         )
       config.registry_public_key = config.registry_private_key.public_key
       config.access_policies = '/etc/rmt/access_policies.yml'
