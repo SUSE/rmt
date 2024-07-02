8425d5ca194c97d814b6525ffa8a294c55f69f8c
Thomas Schmidt <tschmidt@suse.de>
2024-07-02 17:44:06 +0200

    try with sigint

--- features/lockfile_spec.rb
+++ features/lockfile_spec.rb
@@ -1,13 +1,16 @@
-describe 'rmt-cli lock' do
-  around do |example|
-    parent_pid = fork do
-      exec "/usr/bin/rmt-cli sync > /dev/null"
-    end
-    example.run
-    Process.kill('KILL', parent_pid)
-  end
+require File.expand_path('../support/command_rspec_helper', __FILE__)
 
+describe 'rmt-cli' do
   describe 'lockfile' do
+
+    around do |example|
+      parent_pid = fork do
+        exec "/usr/bin/rmt-cli sync > /dev/null"
+      end
+      example.run
+      Process.kill('INT', parent_pid)
+    end
+
     command '/usr/bin/rmt-cli sync', allow_error: true
     its(:stderr) do
       is_expected.to eq(
