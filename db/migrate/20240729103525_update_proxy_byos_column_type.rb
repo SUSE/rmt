139a6dca2fe80c1e013213dc81b65cd41453b86c
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-07-29 16:09:53 +0100

    No need for down block as the column is not removed

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -5,11 +5,4 @@ class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
     execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
     # remove_column :systems, :proxy_byos
   end
-
-  def down
-    add_column :systems, :proxy_byos, :boolean, default: false
-    execute 'Update systems SET proxy_byos = false WHERE proxy_byos_mode = 0'
-    execute 'Update systems SET proxy_byos = true WHERE proxy_byos_mode = 1'
-    # remove_column :systems, :proxy_byos_method
-  end
 end
