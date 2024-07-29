8df5017eaa4ac2f521d773f81b40c3f93cdabc85
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-07-29 16:07:38 +0100

    Remove safety clause

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -1,19 +1,15 @@
 class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   def up
-    safety_assured do
-      add_column :systems, :proxy_byos_mode, :integer, default: 0
-      execute 'Update systems SET proxy_byos_mode = 0 WHERE proxy_byos = false'
-      execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
-      # remove_column :systems, :proxy_byos
-    end
+    add_column :systems, :proxy_byos_mode, :integer, default: 0
+    execute 'Update systems SET proxy_byos_mode = 0 WHERE proxy_byos = false'
+    execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
+    # remove_column :systems, :proxy_byos
   end
 
   def down
-    safety_assured do
-      add_column :systems, :proxy_byos, :boolean, default: false
-      execute 'Update systems SET proxy_byos = false WHERE proxy_byos_mode = 0'
-      execute 'Update systems SET proxy_byos = true WHERE proxy_byos_mode = 1'
-      # remove_column :systems, :proxy_byos_method
-    end
+    add_column :systems, :proxy_byos, :boolean, default: false
+    execute 'Update systems SET proxy_byos = false WHERE proxy_byos_mode = 0'
+    execute 'Update systems SET proxy_byos = true WHERE proxy_byos_mode = 1'
+    # remove_column :systems, :proxy_byos_method
   end
 end
