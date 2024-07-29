913c7dda3bdb2bef8eecfb0dd5f896d937fcdf86
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-07-29 16:04:57 +0100

    Keep the column for testing purposes
    The old column will be removed in a subsequent PR

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -1,19 +1,19 @@
 class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   def up
     safety_assured do
-      add_column :systems, :proxy_byos_method, :integer, default: 0
-      execute 'Update systems SET proxy_byos_method = 0 WHERE proxy_byos = false'
-      execute 'Update systems SET proxy_byos_method = 1 WHERE proxy_byos = true'
-      remove_column :systems, :proxy_byos
+      add_column :systems, :proxy_byos_mode, :integer, default: 0
+      execute 'Update systems SET proxy_byos_mode = 0 WHERE proxy_byos = false'
+      execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
+      # remove_column :systems, :proxy_byos
     end
   end
 
   def down
     safety_assured do
       add_column :systems, :proxy_byos, :boolean, default: false
-      execute 'Update systems SET proxy_byos = false WHERE proxy_byos_method = 0'
-      execute 'Update systems SET proxy_byos = true WHERE proxy_byos_method = 1'
-      remove_column :systems, :proxy_byos_method
+      execute 'Update systems SET proxy_byos = false WHERE proxy_byos_mode = 0'
+      execute 'Update systems SET proxy_byos = true WHERE proxy_byos_mode = 1'
+      # remove_column :systems, :proxy_byos_method
     end
   end
 end
