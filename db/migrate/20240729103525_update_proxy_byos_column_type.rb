c206e42cf710fa219444ec6dc1bd6e27d4ffb33f
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-07-29 18:26:43 +0100

    Adding safety clause back

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -1,8 +1,10 @@
 class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   def up
-    add_column :systems, :proxy_byos_mode, :integer, default: 0
-    execute 'Update systems SET proxy_byos_mode = 0 WHERE proxy_byos = false'
-    execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
+    safety_assured do
+      add_column :systems, :proxy_byos_mode, :integer, default: 0
+      execute 'Update systems SET proxy_byos_mode = 0 WHERE proxy_byos = false'
+      execute 'Update systems SET proxy_byos_mode = 1 WHERE proxy_byos = true'
+    end
     # remove_column :systems, :proxy_byos
   end
 
