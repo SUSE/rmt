5a7d1505fe5c78a370cfb40cb1b66faf11c7cebc
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-07-29 13:37:10 +0100

    Add safety clause

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -1,23 +1,27 @@
 class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   def up
     add_column :systems, :proxy_byos_boolean, :boolean, default: false
-    execute 'Update systems SET proxy_byos_boolean = false WHERE proxy_byos = false'
-    execute 'Update systems SET proxy_byos_boolean = true WHERE proxy_byos = true'
-    remove_column :systems, :proxy_byos
-    add_column :systems, :proxy_byos, :integer, default: 0
-    execute 'Update systems SET proxy_byos = 0 WHERE proxy_byos_boolean = false'
-    execute 'Update systems SET proxy_byos = 1 WHERE proxy_byos_boolean = true'
-    remove_column :systems, :proxy_byos_boolean
+    safety_assured do
+      execute 'Update systems SET proxy_byos_boolean = false WHERE proxy_byos = false'
+      execute 'Update systems SET proxy_byos_boolean = true WHERE proxy_byos = true'
+      remove_column :systems, :proxy_byos
+      add_column :systems, :proxy_byos, :integer, default: 0
+      execute 'Update systems SET proxy_byos = 0 WHERE proxy_byos_boolean = false'
+      execute 'Update systems SET proxy_byos = 1 WHERE proxy_byos_boolean = true'
+      remove_column :systems, :proxy_byos_boolean
+    end
   end
 
   def down
     add_column :systems, :proxy_byos_boolean, :boolean, default: false
-    execute 'Update systems SET proxy_byos_boolean = false WHERE proxy_byos = 0'
-    execute 'Update systems SET proxy_byos_boolean = true WHERE proxy_byos = 1'
-    remove_column :systems, :proxy_byos
-    add_column :systems, :proxy_byos, :boolean, default: false
-    execute 'Update systems SET proxy_byos = false WHERE proxy_byos_boolean = false'
-    execute 'Update systems SET proxy_byos = true WHERE proxy_byos_boolean = true'
-    remove_column :systems, :proxy_byos_boolean
+    safety_assured do
+      execute 'Update systems SET proxy_byos_boolean = false WHERE proxy_byos = 0'
+      execute 'Update systems SET proxy_byos_boolean = true WHERE proxy_byos = 1'
+      remove_column :systems, :proxy_byos
+      add_column :systems, :proxy_byos, :boolean, default: false
+      execute 'Update systems SET proxy_byos = false WHERE proxy_byos_boolean = false'
+      execute 'Update systems SET proxy_byos = true WHERE proxy_byos_boolean = true'
+      remove_column :systems, :proxy_byos_boolean
+    end
   end
 end
