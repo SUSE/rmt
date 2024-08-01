5e91f795e4cf7a32377186c6fdb8ec5d2da16a0b
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-08-01 11:48:48 +0100

    Do not remove the proxy_byos column yet
    We need to make regsharing compatible between RMT versions,
    once all RMT siblings have the new column (proxy_byos_mode)
    present in the DB we can drop that proxy_byos column
    
    The dropping would happen in a different RMT version

--- db/migrate/20240730090211_remove_proxy_byos_boolean.rb
+++ db/migrate/20240730090211_remove_proxy_byos_boolean.rb
@@ -1,11 +1,5 @@
 class RemoveProxyByosBoolean < ActiveRecord::Migration[6.1]
-  def up
-    safety_assured { remove_column :systems, :proxy_byos }
-  end
-
   def down
-    add_column :systems, :proxy_byos, :boolean, default: false
-    System.where(proxy_byos_mode: 0).in_batches.update_all proxy_byos: false
-    System.where(proxy_byos_mode: 1).in_batches.update_all proxy_byos: true
+    System.where(proxy_byos_mode: :byos).in_batches.update_all proxy_byos: true
   end
 end
