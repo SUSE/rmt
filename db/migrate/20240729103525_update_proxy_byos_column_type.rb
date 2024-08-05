21ac00248bb6155d11cd6bf7ed41138efab97ee8
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-08-05 16:09:35 +0100

    Keep consistency with the migration queries

--- db/migrate/20240729103525_update_proxy_byos_column_type.rb
+++ db/migrate/20240729103525_update_proxy_byos_column_type.rb
@@ -1,7 +1,7 @@
 class UpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   def up
     add_column :systems, :proxy_byos_mode, :integer, default: 0
-    System.where('proxy_byos = 0 and instance_data IS NOT NULL').in_batches.update_all proxy_byos_mode: :payg
+    System.where(proxy_byos: false).where.not(instance_data: nil).in_batches.update_all proxy_byos_mode: :payg
     System.where(proxy_byos: true).in_batches.update_all proxy_byos_mode: :byos
   end
 
