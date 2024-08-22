9ce1182ad115bd9096ec38cf2e974f06e74d375f
Jesus Bermudez Velazquez <jesusbv@suse.com>
2024-08-22 10:50:58 +0100

    Use the product info regarding whether is free instead of LTSS

--- engines/registry/app/models/access_scope.rb
+++ engines/registry/app/models/access_scope.rb
@@ -88,22 +88,19 @@ class AccessScope
     allowed_products = (active_products & access_policies_yml.keys)
     allowed_glob_paths = access_policies_yml.values_at(*allowed_products).flatten
 
-    if system && system.hybrid?
-      allowed_product_class = allowed_products.detect { |s| s.downcase.include?('ltss') }
-      if allowed_product_class.present?
-        auth_header = {
-          Authorization: ActionController::HttpAuthentication::Basic.encode_credentials(system.login, system.password)
-        }
-        activation_state = SccProxy.scc_check_subscription_expiration(
-          auth_header, system.login, system.system_token, Rails.logger, system.proxy_byos_mode, allowed_product_class
+    if system && system.hybrid? && allowed_products.any? { |s| !Product.find_by(product_class: s).free? }.present?
+      auth_header = {
+        Authorization: ActionController::HttpAuthentication::Basic.encode_credentials(system.login, system.password)
+      }
+      activation_state = SccProxy.scc_check_subscription_expiration(
+        auth_header, system.login, system.system_token, Rails.logger, system.proxy_byos_mode, allowed_products.first
+        )
+      unless activation_state[:is_active]
+        Rails.logger.info(
+          "Access to #{allowed_products.first} from system #{system.login} denied: #{activation_state[:message]}"
           )
-        unless activation_state[:is_active]
-          Rails.logger.info(
-            "Access to #{allowed_product_class} from system #{system.login} denied: #{activation_state[:message]}"
-          )
-          @allowed_paths = []
-          return
-        end
+        @allowed_paths = []
+        return
       end
     end
     @allowed_paths = parse_repos(repo_list, allowed_glob_paths)
