8a7d37fd06c1aa21874fb39cb835d663fb0d5a84
Natnael Getahun <natnael.getahun@suse.com>
2024-11-05 17:07:51 +0100

    Remove unused rake metrics

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -15,30 +15,11 @@ Yabeda.configure do
   }
 
   group :rails, &assign_labels
-  group :rake, &assign_labels
 
   group :rails do
     counter :started_requests_total,
             comment: 'A counter of the total number of HTTP requests rails has started to process.',
             tags: %i[controller action format method]
-  end
-
-  group :rake do
-    gauge :task_started_at,
-          comment: 'Time when the task started: unix time with decimals',
-          tags: %i[task_name]
-
-    gauge :task_exit_status,
-          comment: 'Task exit status. 1 means OK, 0 means failed, not present means no data',
-          tags: %i[task_name]
-
-    gauge :task_duration_ms,
-          comment: 'Time taken running the task',
-          tags: %i[task_name]
-
-    gauge :task_finished_at,
-          comment: 'Time when the task finished: unix time with decimals',
-          tags: %i[task_name]
   end
 end
 
@@ -52,20 +33,4 @@ ActiveSupport::Notifications.subscribe 'start_processing.action_controller' do |
 
     metric.increment(labels, by: 1)
   end
-end
-
-ActiveSupport::Notifications.subscribe 'task_run.rake' do |*args|
-  event = ActiveSupport::Notifications::Event.new(*args)
-  task_name = event.payload[:task_name]
-
-  success = 1
-  if !!(event.payload[:exception] || event.payload[:exception_class])
-    success = 0
-  end
-
-  Yabeda.rake.task_duration_ms.set({ task_name: task_name }, event.duration)
-  Yabeda.rake.task_started_at.set({ task_name: task_name }, event.time / 1000.0)
-  Yabeda.rake.task_finished_at.set({ task_name: task_name }, event.end / 1000.0)
-
-  Yabeda.rake.task_exit_status.set({ task_name: task_name }, success)
 end
