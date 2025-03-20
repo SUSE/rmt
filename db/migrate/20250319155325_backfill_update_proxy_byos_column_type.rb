e3abf590b1ef7e6fa8e38fb5ac2051c0fbe7d438
Luis Caparroz <lcaparroz@suse.com>
2025-03-20 10:51:20 +0100

    fix: Rewrite migration for proxy byos mode column
    Move the backfill of the 'proxy_byos_mode' column to a separated
    migration. The previous changes on PR #1301 only created a new migration
    to backfill the default value of the column, but the original one still
    contained backfill logic.
    
    Since a backfill migration was created ('strong_migrations'
    recommendation), this commit just aggregates all the backfill logic into
    a single migration, keeping column creation and updating its values
    separated.

--- db/migrate/20250319155325_backfill_update_proxy_byos_column_type.rb
+++ db/migrate/20250319155325_backfill_update_proxy_byos_column_type.rb
@@ -2,7 +2,17 @@ class BackfillUpdateProxyByosColumnType < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def up
-    System.unscoped.in_batches do |relation|
+    System.where(proxy_byos: false).where.not(instance_data: nil).in_batches do |relation|
+      relation.update_all proxy_byos_mode: :payg
+      sleep(0.01)
+    end
+
+    System.where(proxy_byos: true).in_batches do |relation|
+      relation.update_all proxy_byos_mode: :byos
+      sleep(0.01)
+    end
+
+    System.unscoped.where(proxy_byos_mode: nil).in_batches do |relation|
       relation.update_all proxy_byos_mode: 0
       sleep(0.01)
     end
