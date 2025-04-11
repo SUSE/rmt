c6537a175ba03dbabee03ace38ea53930b4c8b7e
Jesus Bermudez Velazquez <jesusbv@suse.com>
2025-04-11 14:52:44 +0100

    Export data when system activates or upgrades a product

--- engines/data_export/lib/data_export/engine.rb
+++ engines/data_export/lib/data_export/engine.rb
@@ -48,6 +48,28 @@ module DataExport
           logger.error(e.backtrace)
         end
       end
+
+      Api::Connect::V3::Systems::ProductsController.class_eval do
+        after_action :export_rmt_data, only: %i[activate upgrade], if: -> { response.successful? && !@system.byos? }
+
+        def export_rmt_data
+          params[:product_triplet] = @product.product_string
+          data_export_handler = DataExport.handler.new(
+            @system,
+            request,
+            params,
+            logger
+          )
+          data_export_handler.export_rmt_data
+          logger.info "System #{@system.login} info updated by data export handler after activating or updating the product #{@product.product_string}"
+        rescue StandardError => e
+          logger.error('Unexpected data export error has occurred:')
+          logger.error(e.message)
+          logger.error("System login: #{@system.login}, IP: #{request.remote_ip}")
+          logger.error('Backtrace:')
+          logger.error(e.backtrace)
+        end
+      end
     end
   end
 end
