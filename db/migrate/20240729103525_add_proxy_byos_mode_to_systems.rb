bc59b31ba881339e7e1180aa93986221001e4c0b
Thomas Schmidt <tschmidt@suse.de>
2025-07-08 14:29:36 +0200

    be more failsafe when local db already has columns

--- db/migrate/20240729103525_add_proxy_byos_mode_to_systems.rb
+++ db/migrate/20240729103525_add_proxy_byos_mode_to_systems.rb
@@ -1,10 +1,10 @@
 class AddProxyByosModeToSystems < ActiveRecord::Migration[6.1]
   def up
-    add_column :systems, :proxy_byos_mode, :integer
+    add_column :systems, :proxy_byos_mode, :integer, if_exists: false
     change_column_default :systems, :proxy_byos_mode, 0
   end
 
   def down
-    remove_column :systems, :proxy_byos_mode
+    remove_column :systems, :proxy_byos_mode, if_exists: true
   end
 end
