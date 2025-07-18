8624973f2bd142982ea810f3ccb320cce1e92d64
Jesus Bermudez Velazquez <jesusbv@suse.com>
2025-07-18 15:43:15 -0400

    Use instance id as a login on the public cloud environment
    Instance id is guarantee to be unique across the three CSPs,
    thus we do not need to generate a pseudo unique login value
    
    This would help also with duplicated systems detection

--- app/models/system.rb
+++ app/models/system.rb
@@ -20,6 +20,11 @@ class System < ApplicationRecord
   alias_attribute :scc_synced_at, :scc_registered_at
 
   def init
+    # if we are in the public cloud
+    # system_token should be not nil
+    self.login ||= system_token if system_token.presence
+    # if we are not in the public cloud
+    # use the regular SCC_UUID generated value
     self.login ||= System.generate_secure_login
     self.password ||= System.generate_secure_password
     self.registered_at ||= Time.zone.now
