eac4f90d1f6162f2f00f93aae789798f458b4621
Fergal Mc Carthy <fmccarthy@suse.com>
2025-11-21 17:05:36 -0500

    Fix typo (FOR => IN) in previous commit

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -87,10 +87,10 @@ class Profile < ApplicationRecord
     # Only one concurrent create will succeed, others will raise this
     # exception, so just find the newly created record and return it,
     # locking the query to ensure that the latest table content is used.
-    # NOTE: To avoid deadlocks we need to use LOCK FOR SHARE MODE to
+    # NOTE: To avoid deadlocks we need to use LOCK IN SHARE MODE to
     # ensure that the generated SQL specifies FOR SHARE (shared locking)
     # rather than FOR UPDATE (exclusive locking)
-    profile = profile_query.lock('LOCK FOR SHARE MODE').first!
+    profile = profile_query.lock('LOCK IN SHARE MODE').first!
     logger.debug("ensure_profile_exists: found(rescue) profile - #{profile.profile_type}/#{profile.identifier}")
     profile
   end
