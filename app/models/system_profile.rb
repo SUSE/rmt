98da3517521ecf4a01175b4ca714dbc12b653195
Fergal Mc Carthy <fmccarthy@suse.com>
2026-01-05 16:13:19 -0500

    Address review feedback.
    Add an explanatory comment as to why locking is used in the orphaned
    profile destroy handler.
    
    Add a log message in the rescue block for the case where a profile
    entry was already destroyed by a racing orphaned profile destroy
    handler.

--- app/models/system_profile.rb
+++ app/models/system_profile.rb
@@ -6,6 +6,17 @@ class SystemProfile < ApplicationRecord
   # callbacks
   after_commit :destroy_orphaned_profile, on: :destroy
 
+  # NOTE: Without locking, racing destroy handlers may detect each
+  # others profile entry references via the system_profiles table
+  # and thus decide to skip deleting the associated profile entry.
+  # Using locking serializes the reference checking, potentially
+  # mitigating any staleness issues that can arrise due to DB backend
+  # optimisations, meaning that checks are performed against actual
+  # table state, ensuring that at least one handler will see that all
+  # other references have been removed and thus correctly decide to
+  # delete the orphaned profile.
+  # As such this approach should eliminate the need to schedule a
+  # background job to detect and delete orphaned profile entries.
   def destroy_orphaned_profile
     # if another racing orphan deletion has already deleted this
     # profile entry then the in memory reference may now be nil,
@@ -24,5 +35,6 @@ class SystemProfile < ApplicationRecord
   rescue ActiveRecord::RecordNotFound
     # another racing orphan cleanup has already deleted this
     # profile so we don't need to do anything
+    logger.debug('orphaned profile already deleted by another racing destroy handler')
   end
 end
