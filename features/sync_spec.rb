0145c8137a6e554f0e58e19cc06214a89212605c
Adnilson <adnilson.delgado@suse.com>
2026-01-12 10:56:23 +0000

    Use shellout to run sync command

--- features/sync_spec.rb
+++ features/sync_spec.rb
@@ -1,7 +1,11 @@
+require 'mixlib/shellout'
+
 describe 'rmt-cli' do
   describe 'sync' do
-    system '/usr/bin/rmt-cli sync'
-    # its(:exitstatus) { is_expected.to eq 0 }
+    it do
+      command = Mixlib::ShellOut.new("/usr/bin/rmt-cli sync").run_command
+      expect(command.exitstatus).to eq(0)
+    end
 
     it do
       expect(`/usr/bin/rmt-cli products list --all | wc -l`.strip&.to_i ).to be >= 361
