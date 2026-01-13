3dbf8a73a08a509992d520eac3f5b15a263a6aa6
James Mason <jmason@suse.com>
2026-01-13 15:12:13 -0800

    Set Sidekiq as the ActiveJob Queue Adapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rmt_production"
+  config.active_job.queue_adapter     = :sidekiq
+  config.active_job.queue_name_prefix = "rmt_production"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
