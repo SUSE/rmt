2ccc18e550ff3698e655b48903fe7d6e34d3687c
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-01-15 12:18:11 +0000

    Clean tests
    Remove extra fake data
    Remove comments

--- db/migrate/20260108092033_add_index_to_systems_hostname.rb
+++ db/migrate/20260108092033_add_index_to_systems_hostname.rb
@@ -1,4 +1,4 @@
-class AddIndexToSystemsHostname < ActiveRecord::Migration[5.1]
+class AddIndexToSystemsHostname < ActiveRecord::Migration[8.1]
   def change
     add_index :systems, :hostname, unique: false
   end
