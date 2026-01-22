6cfa138b02f50105cad403cbb356e1a5f66ade8d
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-01-22 11:57:21 +0000

    Adjust spaces per cell
    The table add space equally to all the cells,
    producing cells that have a lot of extra space
    
    To mitigate this regression, this change add space per cell,
    making the width per cell and not equally applied to the whole table

--- lib/rmt/cli/decorators/system_decorator.rb
+++ lib/rmt/cli/decorators/system_decorator.rb
@@ -17,9 +17,11 @@ class RMT::CLI::Decorators::SystemDecorator < RMT::CLI::Decorators::Base
     array_to_csv(systems, HEADERS, batch: batch)
   end
 
-  def to_table(add_headers: true, style: {})
-    systems = systems_to_arrays(join_new_line: true)
+  def to_table(add_headers: true, style: {}, width: [0, 0, 0, 0, 0])
+    systems = systems_to_arrays(join_new_line: true, width: width)
     headers = add_headers ? HEADERS : []
+    width.each_with_index { |col_width, index| headers[index].ljust(col_width) } if headers.present? && width[0] != 0
+
     array_to_table(systems, headers, style)
   end
 
@@ -27,23 +29,23 @@ class RMT::CLI::Decorators::SystemDecorator < RMT::CLI::Decorators::Base
 
   attr_reader :data
 
-  def systems_to_arrays(join_new_line: false)
+  def systems_to_arrays(join_new_line: false, width: [0, 0, 0, 0, 0])
     data.map do |system|
       [
-        system.login,
-        system.hostname,
-        system.registered_at,
-        system.last_seen_at,
-        products_and_subscriptions(system, join_new_line: join_new_line)
+        system.login.ljust(width[0]),
+        system.hostname.ljust(width[1]),
+        system.registered_at.to_s.ljust(width[2]),
+        system.last_seen_at.to_s.ljust(width[3]),
+        products_and_subscriptions(system, join_new_line: join_new_line, width: width[4])
       ]
     end
   end
 
-  def products_and_subscriptions(system, join_new_line: false)
+  def products_and_subscriptions(system, join_new_line: false, width: 0)
     products = system.activations.map do |a|
       product_name = a.service.product.product_string
 
-      a.subscription ? "#{product_name} (Regcode: #{a.subscription.regcode})" : product_name
+      a.subscription ? "#{product_name.ljust(width)}\n(Regcode: #{a.subscription.regcode.ljust(width)})" : product_name.ljust(width)
     end
 
     join_new_line ? products.join("\n") : products.join(' ')
