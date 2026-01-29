943a75ccde4b7517e4ac157f554f301bf500d962
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-01-29 13:03:10 +0000

    Empty the entry cache for the system with wrong instance data
    if the system instance data stored in the DB can not be parsed,
    we remove that entry cache, if present, so that it can be updated

--- engines/zypper_auth/spec/requests/strict_authentication/authentication_controller_spec.rb
+++ engines/zypper_auth/spec/requests/strict_authentication/authentication_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 describe StrictAuthentication::AuthenticationController, type: :request do
   subject { response }
 
-  let(:system) { FactoryBot.create(:system, :payg, :with_activated_product) }
+  let(:system) { FactoryBot.create(:system, :hybrid, :with_activated_product) }
 
   after { FileUtils.rm_rf(File.dirname(Rails.application.config.registry_cache_dir)) }
 
@@ -89,30 +89,68 @@ describe StrictAuthentication::AuthenticationController, type: :request do
         end
       end
 
-      context 'with instance_data headers and instance data is invalid in the DB' do
-        let(:headers) { auth_header.merge({ 'X-Original-URI': requested_uri, 'X-Instance-Data': Base64.strict_encode64('test') }) }
-        let(:data_export_double) { instance_double('DataExport::Handlers::Example') }
-        let(:wrong_iid) { '<repoformat>plugin:susecloud</repoformat>\n' }
+      context 'system with instance_data headers instance data is invalid in the DB' do
+        context 'InstanceVerification error' do
+          let(:headers) { auth_header.merge({ 'X-Original-URI': requested_uri, 'X-Instance-Data': Base64.strict_encode64('test') }) }
+          let(:data_export_double) { instance_double('DataExport::Handlers::Example') }
+          let(:wrong_iid) { '<repoformat>plugin:susecloud</repoformat>\n' }
 
 
-        before do
-          allow(InstanceVerification).to receive(:reg_code_in_cache?).and_return(nil)
-          Rails.cache.clear
-          expect_any_instance_of(InstanceVerification::Providers::Example).to receive(:instance_valid?).and_return(true)
-          allow(DataExport::Handlers::Example).to receive(:new).and_return(data_export_double)
-          allow(File).to receive(:directory?)
-          allow(Dir).to receive(:mkdir)
-          allow(FileUtils).to receive(:touch)
-          expect(data_export_double).to receive(:export_rmt_data).and_raise(InstanceVerification::Exception, "Malformed instance data #{wrong_iid}")
-          allow(Rails.logger).to receive(:error)
-          expect(Rails.logger).to receive(:error).with('Unexpected data export error has occurred:')
-          expect(Rails.logger).to receive(:error).with('Malformed instance data <repoformat>plugin:susecloud</repoformat>\n')
-          expect(Rails.logger).to receive(:error).with("System login: #{system.login}, IP: 127.0.0.1")
-          get '/api/auth/check', headers: headers
+          before do
+            allow(InstanceVerification).to receive(:reg_code_in_cache?).and_return(nil)
+            Rails.cache.clear
+            expect_any_instance_of(InstanceVerification::Providers::Example).to receive(:instance_valid?).and_return(true)
+            allow(DataExport::Handlers::Example).to receive(:new).and_return(data_export_double)
+            allow(File).to receive(:directory?)
+            allow(Dir).to receive(:mkdir)
+            allow(FileUtils).to receive(:touch)
+            expect(data_export_double).to receive(:export_rmt_data).and_raise(InstanceVerification::Exception, "Malformed instance data #{wrong_iid}")
+            allow(Rails.logger).to receive(:error)
+            allow(Rails.logger).to receive(:info)
+            expect(Rails.logger).to receive(:error).with(
+              'Could not parse the instance data: Malformed instance data <repoformat>plugin:susecloud</repoformat>\n'
+            )
+            expect(Rails.logger).to receive(:info)
+            get '/api/auth/check', headers: headers
+          end
+
+          it do
+            is_expected.to have_http_status(200)
+          end
         end
 
-        it do
-          is_expected.to have_http_status(403)
+        context 'StandardError' do
+          let(:system_byos) { FactoryBot.create(:system, :byos_reg_code, :with_activated_product) }
+          let(:headers) { auth_header.merge({ 'X-Original-URI': requested_uri, 'X-Instance-Data': Base64.strict_encode64('test') }) }
+          let(:data_export_double) { instance_double('DataExport::Handlers::Example') }
+          let(:wrong_iid) { '<repoformat>plugin:susecloud</repoformat>\n' }
+
+
+          before do
+            allow(InstanceVerification).to receive(:reg_code_in_cache?).and_return(nil)
+            Rails.cache.clear
+            expect_any_instance_of(InstanceVerification::Providers::Example).to receive(:instance_valid?).and_return(true)
+            allow(DataExport::Handlers::Example).to receive(:new).and_return(data_export_double)
+            allow(File).to receive(:directory?)
+            allow(Dir).to receive(:mkdir)
+            allow(FileUtils).to receive(:touch)
+            expect(data_export_double).to receive(:export_rmt_data).and_raise(
+              StandardError, "API BORKED"
+            )
+            allow(Rails.logger).to receive(:error)
+            expect(Rails.logger).to receive(:error).with(
+              'Unexpected data export error has occurred: API BORKED'
+            )
+            expect(Rails.logger).to receive(:error).with('Data not exported')
+            expect(Rails.logger).to receive(:error).with(
+              "System login: #{system.login}, IP: 127.0.0.1"
+            )
+            get '/api/auth/check', headers: headers
+          end
+
+          it do
+            is_expected.to have_http_status(200)
+          end
         end
       end
 
