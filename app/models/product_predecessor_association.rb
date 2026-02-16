c9daf1c8c0c09c8b2680245dd3c4fc986c2326cb
Natnael Getahun <natnael.getahun@suse.com>
2026-02-16 10:15:10 +0100

    - Fix hash based enum calling to keyword arguments - Take care of deprecation

--- app/models/product_predecessor_association.rb
+++ app/models/product_predecessor_association.rb
@@ -1,7 +1,7 @@
 class ProductPredecessorAssociation < ApplicationRecord
   self.table_name = 'product_predecessors'
 
-  enum kind: { online: 0, offline: 1 }
+  enum :kind, { online: 0, offline: 1 }
 
   validates :product_id, :predecessor_id, :kind, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,52 +2,56 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :connect, module: 'api/connect', defaults: { format: 'json' } do
-    api_version(module: 'V4', header: { name: 'Accept', value: 'application/vnd.scc.suse.com.v4+json' }, default: true) do
-      scope :subscriptions, module: :subscriptions do
-        post 'systems', to: 'systems#announce_system'
-        resources :products, only: [:index]
+    Rails.application.deprecators[:action_dispatch].silence do
+      api_version(module: 'V4', header: { name: 'Accept', value: 'application/vnd.scc.suse.com.v4+json' }, default: true) do
+        scope :subscriptions, module: :subscriptions do
+          post 'systems', to: 'systems#announce_system'
+          resources :products, only: [:index]
+        end
+
+        put 'systems', to: 'systems/systems#update'
+        delete 'systems', to: 'systems/systems#deregister'
+
+        scope :systems, module: :systems, as: 'systems' do
+          get 'products', to: 'products#show'
+          post 'products', to: 'products#activate'
+          put 'products', to: 'products#upgrade'
+          delete 'products', to: 'products#destroy'
+          post 'products/migrations', to: 'products#migrations'
+          post 'products/offline_migrations', to: 'products#offline_migrations'
+          post 'products/synchronize', to: 'products#synchronize'
+        end
+
+        resource :systems, only: [], module: :systems do
+          resources :activations, only: [ :index ]
+        end
+
+        get 'repositories/installer', to: 'repositories/installer#index'
       end
-
-      put 'systems', to: 'systems/systems#update'
-      delete 'systems', to: 'systems/systems#deregister'
-
-      scope :systems, module: :systems, as: 'systems' do
-        get 'products', to: 'products#show'
-        post 'products', to: 'products#activate'
-        put 'products', to: 'products#upgrade'
-        delete 'products', to: 'products#destroy'
-        post 'products/migrations', to: 'products#migrations'
-        post 'products/offline_migrations', to: 'products#offline_migrations'
-        post 'products/synchronize', to: 'products#synchronize'
-      end
-
-      resource :systems, only: [], module: :systems do
-        resources :activations, only: [ :index ]
-      end
-
-      get 'repositories/installer', to: 'repositories/installer#index'
     end
   end
 
   namespace :connect, module: 'api/connect', defaults: { format: 'json' } do
-    api_version(module: 'V3', header: { name: 'Accept', value: 'application/vnd.scc.suse.com.v3+json' }) do
-      scope :subscriptions, module: :subscriptions do
-        post 'systems', to: 'systems#announce_system'
-      end
+    Rails.application.deprecators[:action_dispatch].silence do
+      api_version(module: 'V3', header: { name: 'Accept', value: 'application/vnd.scc.suse.com.v3+json' }) do
+        scope :subscriptions, module: :subscriptions do
+          post 'systems', to: 'systems#announce_system'
+        end
 
-      put 'systems', to: 'systems/systems#update'
-      delete 'systems', to: 'systems/systems#deregister'
+        put 'systems', to: 'systems/systems#update'
+        delete 'systems', to: 'systems/systems#deregister'
 
-      scope :systems, module: :systems, as: 'systems' do
-        get 'products', to: 'products#show'
-        post 'products', to: 'products#activate'
-        put 'products', to: 'products#upgrade'
-        post 'products/migrations', to: 'products#migrations'
-        post 'products/offline_migrations', to: 'products#offline_migrations'
-      end
+        scope :systems, module: :systems, as: 'systems' do
+          get 'products', to: 'products#show'
+          post 'products', to: 'products#activate'
+          put 'products', to: 'products#upgrade'
+          post 'products/migrations', to: 'products#migrations'
+          post 'products/offline_migrations', to: 'products#offline_migrations'
+        end
 
-      resource :systems, only: [], module: :systems do
-        resources :activations, only: [ :index ]
+        resource :systems, only: [], module: :systems do
+          resources :activations, only: [ :index ]
+        end
       end
     end
   end
