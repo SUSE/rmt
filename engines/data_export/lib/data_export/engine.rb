b99e388eafcc5afb5d0f81a47d95db827e73ed2b
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-02-26 16:42:55 +0000

    Do not populate the DW file inside system update at registration
    When the client runs a registration multiple endpoints are called,
    one of them is the system update while the products of the system are
    being activated
    
    if both endpoints callbacks write to the file, it generates duplicate
    records
    
    We could use the system update only to update the file to be exported
    to DW but the problem is the information is not complete when the
    endpoint is invoked, meaning there would be duplicate records using
    that endpoint information because of how registration works
    
    The propose change uses the product activate endpoint to populate the
    file whenever there is a product activation or upgrade (for example,
    registration) and system update would populate the file when is
    invoked outside a registration also
    
    This fixes bsc#1258943

--- engines/data_export/lib/data_export/engine.rb
+++ engines/data_export/lib/data_export/engine.rb
@@ -2,39 +2,16 @@ module DataExport
   class Engine < ::Rails::Engine
     isolate_namespace DataExport
     config.after_initialize do
-      # replaces RMT registration for SCC registration
-      Api::Connect::V3::Subscriptions::SystemsController.class_eval do
-        after_action :export_rmt_data, only: %i[announce_system], if: -> { DataExport.handler.presence && response.successful? && !@system.byos? }
-
-        def export_rmt_data
-          # no need to check if system is nil
-          # as the response is successful
-          data_export_handler = DataExport.handler.new(
-            @system,
-            request,
-            params,
-            logger
-          )
-          data_export_handler.export_rmt_data
-          logger.info "System #{@system.login} info updated by data export handler after announcing the system"
-        rescue StandardError => e
-          logger.error('Unexpected data export error has occurred:')
-          logger.error(e.message)
-          logger.error("System login: #{@system.login}, IP: #{request.remote_ip}")
-          logger.error('Backtrace:')
-          logger.error(e.backtrace)
-        end
-      end
-
       Api::Connect::V3::Systems::SystemsController.class_eval do
+        # if registered_at and last_seen_at are the same, it is a registration
         after_action :export_rmt_data, only: %i[update], if: lambda {
           DataExport.handler.presence && response.successful? && !@system.byos? &&
-          @system.products.present?
+          @system.products.present? && @system.registered_at != @system.last_seen_at
         }
 
         def export_rmt_data
-          @system.products.pluck(:name).each do |prod_name|
-            params[:dw_product_name] = prod_name
+          @system.activations.each do |activation|
+            params[:dw_product_name] = activation.service.product.product_string
             send_data
           end
         rescue StandardError => e
