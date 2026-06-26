9c5daf2f2e1751b2c1e0c9a679c508adcb0a2541
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-06-26 16:33:47 +0100

    change data column type in profile table
    currently the data column type is TEXT
    
    TEXT 65535 max length in bytes
    
    and this changes the type to MEDIUMTEXT
    
    MEDIUMTEXT 16777215 max length in bytes
    
    which it could be less depending on the encoding
    
    This Fixes bsc#1268305

---
+++ db/migrate/20260626153118_change_data_type.rb
@@ -0,0 +1,12 @@
+class ChangeDataType < ActiveRecord::Migration[8.1]
+  def up
+    # change the column to MEDIUMTEXT (limit of 16 MB)
+    safety_assured do
+      change_column :profiles, :data, :text, limit: 16.megabytes
+    end
+  end
+
+  def down
+    change_column :profiles, :data, :text
+  end
+end
