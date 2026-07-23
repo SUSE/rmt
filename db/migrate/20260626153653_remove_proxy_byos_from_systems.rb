283d2641638411e9f79880b6a88e8b824804b65c
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-07-23 12:54:14 +0100

    drop the column if exists

--- db/migrate/20260626153653_remove_proxy_byos_from_systems.rb
+++ db/migrate/20260626153653_remove_proxy_byos_from_systems.rb
@@ -1,5 +1,7 @@
 class RemoveProxyByosFromSystems < ActiveRecord::Migration[8.1]
   def change
-    safety_assured { remove_column :systems, :proxy_byos, :boolean, default: false }
+    if column_exists?(:systems, :proxy_byos)
+      remove_column :systems, :proxy_byos, :boolean, default: false
+    end
   end
 end
