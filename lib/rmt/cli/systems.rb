e06620f0c4f274b8c1bb935c4d29b5d64157ce22
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-08-13 10:47:46 +0100

    retry per batch rescue per run

--- lib/rmt/cli/systems.rb
+++ lib/rmt/cli/systems.rb
@@ -123,15 +123,13 @@ class RMT::CLI::Systems < RMT::CLI::Base
     # query systems directly, thus we do not know how
     # many systems before running the deletion
     System.where('last_seen_at < ?', before).in_batches(of: DELETE_BATCH_SIZE) do |batch|
-      begin
-        with_retries { n_systems_destroyed += destroy_batch(batch) }
-      rescue StandardError => e
-        puts _('Could not delete all systems last seen before %{before}: %{message}') % { before: before, message: e.message }
-        [n_systems_destroyed, false]
-      end
+      with_retries { n_systems_destroyed += destroy_batch(batch) }
       puts _('%{n_systems_destroyed} systems destroyed') % { n_systems_destroyed: n_systems_destroyed }
     end
     [n_systems_destroyed, true]
+  rescue StandardError => e
+    puts _('Could not delete all systems last seen before %{before}: %{message}') % { before: before, message: e.message }
+    [n_systems_destroyed, false]
   end
 
   def get_all_matches(before)
@@ -148,23 +146,19 @@ class RMT::CLI::Systems < RMT::CLI::Base
 
   def delete_systems_by_id(system_ids)
     deleted_systems = 0
-    all = true
 
     system_ids.each_slice(DELETE_BATCH_SIZE) do |sliced_systems_ids|
-      begin
-        with_retries { deleted_systems += destroy_batch(System.where(id: sliced_systems_ids)) }
-      rescue StandardError => e
-        remaining_systems = system_ids.length - deleted_systems
-        puts _(
-          'Error while purging the systems: %{error_class} %{message}, all %{all} systems could not be removed, %{remaining} systems still in the database'
-          ) % { error_class: e.class, message: e.message, all: system_ids.length, remaining: remaining_systems }
-        all = false
-        break
-      end
+      with_retries { deleted_systems += destroy_batch(System.where(id: sliced_systems_ids)) }
       remaining_systems = system_ids.length - deleted_systems
       puts _('%{remaining_systems} systems to be deleted') % { remaining_systems: remaining_systems } unless remaining_systems.zero?
     end
-    [deleted_systems, all]
+    [deleted_systems, true]
+  rescue StandardError => e
+    remaining_systems = system_ids.length - deleted_systems
+    puts _(
+      'Error while purging the systems: %{error_class} %{message}, all %{all} systems could not be removed, %{remaining} systems still in the database'
+      ) % { error_class: e.class, message: e.message, all: system_ids.length, remaining: remaining_systems }
+    [deleted_systems, false]
   end
 
   def destroy_batch(relation)
