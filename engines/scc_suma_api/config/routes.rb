bedddb20ffdf43572f3af9ef3175d1cfd3ace2c5
Jesus Bermudez Velazquez <jesusbv@suse.com>
2026-09-18 11:20:09 +0100

    change the endpoint method

--- engines/scc_suma_api/config/routes.rb
+++ engines/scc_suma_api/config/routes.rb
@@ -1,6 +1,6 @@
 SccSumaApi::Engine.routes.draw do
   get 'unscoped-products', to: 'scc_suma_api#unscoped_products'
-  get 'repos', to: 'scc_suma_api#list'
+  get 'repos', to: 'scc_suma_api#repos'
   get 'subs', to: 'scc_suma_api#list'
   get 'orders', to: 'scc_suma_api#list'
   get 'product-tree', to: 'scc_suma_api#product_tree'
